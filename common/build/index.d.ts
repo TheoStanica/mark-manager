fa945a8e392e8a54b2b871734b5571e25577932e
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-09-26 14:58:49 +0300

    'Updates'

--- common/build/index.d.ts
+++ common/build/index.d.ts
@@ -1,2 +1,3 @@
+export * from './middlewares/error-handler';
 export * from './errors/custom-error';
 export * from './errors/not-found-error';
--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,4 @@
-export * from './middlewares/error-handler';
-
 export * from './errors/custom-error';
 export * from './errors/not-found-error';
+
+export * from './middlewares/error-handler';
