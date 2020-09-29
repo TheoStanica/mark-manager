f609ea6d8b55f482f5c265281fa1f17f10c40ff8
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-09-29 16:23:11 +0300

    'Updates'

--- common/build/middlewares/require-auth.d.ts
+++ common/build/middlewares/require-auth.d.ts
@@ -1,9 +1,2 @@
 import { Request, Response, NextFunction } from 'express';
-declare global {
-    namespace Express {
-        interface Request {
-            user?: string[];
-        }
-    }
-}
 export declare const requireAuth: (req: Request, res: Response, next: NextFunction) => void;
--- common/build/middlewares/require-auth.js
+++ common/build/middlewares/require-auth.js
@@ -2,6 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.requireAuth = void 0;
 var not_authorized_error_1 = require("../errors/not-authorized-error");
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: string[];
+//     }
+//   }
+// }
 exports.requireAuth = function (req, res, next) {
     if (!req.user) {
         throw new not_authorized_error_1.NotAuthorizedError();
