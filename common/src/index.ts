4216eece7f25afb0b5ba7f27bf3a305f84c2c965
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-10-05 11:30:41 +0300

    'Updates'

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,6 +4,7 @@ export * from './errors/bad-request-error';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/request-validation-error';
+export * from './errors/forbidden-error';
 
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
