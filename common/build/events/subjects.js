4a275d70dd219060aa91481d23e6487aefd449e6
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-12-24 22:24:41 +0200

    pushed new version of npm package

---
+++ common/build/events/subjects.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Subjects = void 0;
+var Subjects;
+(function (Subjects) {
+    Subjects["UserCreated"] = "user:created";
+})(Subjects = exports.Subjects || (exports.Subjects = {}));
