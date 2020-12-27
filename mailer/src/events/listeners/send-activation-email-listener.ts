f215084e333ce28a769a23d470d7054d88dd3d40
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-12-27 15:50:27 +0200

    moved nodemailer logic outside event listener

--- mailer/src/events/listeners/send-activation-email-listener.ts
+++ mailer/src/events/listeners/send-activation-email-listener.ts
@@ -1,7 +1,7 @@
 import { Listener, SendActivationEmail, Subjects } from '@tcosmin/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
-import nodemailer from 'nodemailer';
+import { transporter } from '../../services/transporter';
 
 export class SendActivationEmailListener extends Listener<SendActivationEmail> {
   subject: Subjects.SendActivationEmail = Subjects.SendActivationEmail;
@@ -9,22 +9,7 @@ export class SendActivationEmailListener extends Listener<SendActivationEmail> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: SendActivationEmail['data'], msg: Message) {
-    // TODO create a class for all of this?
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      auth: {
-        user: 'meet.mark.project@gmail.com',
-        pass: process.env.GMAIL_PASSWORD,
-      },
-    });
-
-    // TODO template for html message?
-    await transporter.sendMail({
-      from: 'Project Mark',
-      to: data.email,
-      subject: 'Project Mark Account Activation',
-      html: `<h1>Welcome to Project Mark!</h1><p>To activate your account, please click <a href="https://mark.dev/api/users/activation/${data.activationToken}">here</a>`,
-    });
+    transporter.sendActivationEmail(data.email, data.activationToken);
 
     msg.ack();
   }
