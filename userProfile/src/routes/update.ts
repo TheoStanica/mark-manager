9baa42493fd35c17af14075a2c6003c5ecdc9e8f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-12-29 22:27:06 +0200

    added option to update user email from this route

--- userProfile/src/routes/update.ts
+++ userProfile/src/routes/update.ts
@@ -3,6 +3,8 @@ import { BadRequestError, requireAuth, validateRequest } from '@tcosmin/common';
 import { body } from 'express-validator';
 import { UserProfileController } from '../controllers/userprofile-controller';
 import { UserDoc, UserProfile } from '../models/userprofile';
+import { EmailChangedPublisher } from '../events/publishers/email-changed-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -12,23 +14,35 @@ router.put(
   [
     body('fullName').isLength({ min: 4, max: 30 }).optional(),
     body('profilePicture').isURL().optional(),
+    body('email').isEmail().optional(),
   ],
 
   validateRequest,
   async (req: Request, res: Response) => {
-    const { fullName, profilePicture } = req.body as UserDoc;
+    const { fullName, profilePicture, email } = req.body as UserDoc;
+
+    if (email && (await UserProfileController.emailExists(email))) {
+      throw new BadRequestError('An user already has this email');
+    }
 
     const user = await UserProfileController.findUserWithId(
       req.currentUser!.userId
     );
-
     const updatedUser = await UserProfileController.updateUser(
       req.currentUser!.userId,
       {
         fullName: fullName ? fullName : user!.fullName,
         profilePicture: profilePicture ? profilePicture : user!.profilePicture,
+        email: email ? email : user!.email,
       }
     );
+
+    await new EmailChangedPublisher(natsWrapper.client).publish({
+      userId: user!.id,
+      email: email,
+    });
+
+    // TODO send email to user about user details updated
 
     res.send({ updatedUser });
   }
