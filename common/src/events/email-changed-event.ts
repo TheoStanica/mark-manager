65e161760b6760baf2649b7f53ee490051f5b14a
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-12-29 21:22:58 +0200

    'Updates'

---
+++ common/src/events/email-changed-event.ts
@@ -0,0 +1,10 @@
+import { Subjects } from './subjects';
+
+export interface EmailChangedEvent {
+  subject: Subjects.EmailChanged;
+
+  data: {
+    userId: string;
+    email: string;
+  };
+}
--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -4,4 +4,6 @@ export enum Subjects {
   SendActivationEmail = 'auth:sendActivationEmail',
 
   AccessTokenRevoked = 'auth:accessToken_revoked',
+
+  EmailChanged = 'userprofile:email_changed',
 }
