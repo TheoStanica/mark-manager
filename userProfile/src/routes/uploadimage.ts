df8f9c8dfea28f91146b351d7e74e76e1ff5e78a
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-16 22:56:04 +0200

    added authenticated user requirement

--- userProfile/src/routes/uploadimage.ts
+++ userProfile/src/routes/uploadimage.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
-import { BadRequestError } from '@tcosmin/common';
+import { BadRequestError, requireAuth } from '@tcosmin/common';
 import { AWSImageUploader } from '../services/aws-image-upload';
 
 const router = express.Router();
 
 router.post(
   '/api/user/uploadimage',
+  requireAuth,
   AWSImageUploader.upload.single('image'),
   async (req: Request, res: Response) => {
     if (!req.file) {
