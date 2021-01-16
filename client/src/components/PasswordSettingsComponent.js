122a00e9ed6bcf4a5ee4eb5314d14a77be9917e3
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-16 22:56:38 +0200

    added logic to change password

--- client/src/components/PasswordSettingsComponent.js
+++ client/src/components/PasswordSettingsComponent.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axiosInstance from '../api/buildClient';
 
-const PasswordSettingsComponent = () => {
+const PasswordSettingsComponent = ({ user }) => {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [repeatNewPassword, setRepeatNewPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('submit form');
+    if (newPassword !== repeatNewPassword) {
+      setErrors(
+        <div className="alert alert-danger">Passwords do not match</div>
+      );
+    } else {
+      try {
+        await axiosInstance.put('/api/auth/changepassword', {
+          currentPassword: oldPassword,
+          newPassword: newPassword,
+        });
+        setMessage(
+          <div className="alert alert-success">Password Changed!</div>
+        );
+        setErrors('');
+      } catch (err) {
+        setErrors(
+          <div className="alert alert-danger">
+            <ul>
+              {err.response.data.errors.map((err) => (
+                <li key={err.message}>{err.message}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      }
+    }
+  };
+
   return (
     <div className="card">
       <div className="card-header">Change Password</div>
       <div className="card-body">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col">
+            <div className="col mb-2">
               <div className="form-group">
                 <label htmlFor="inputFullname">Old Password</label>
-                <input className="form-control" id="inputOldPassword"></input>
+                <input
+                  className="form-control"
+                  id="inputOldPassword"
+                  type="password"
+                  value={oldPassword}
+                  onChange={(e) => setOldPassword(e.target.value)}
+                ></input>
               </div>
               <div className="form-group">
                 <label htmlFor="inputNewPassword">New Password</label>
-                <input className="form-control" id="inputNewPassword"></input>
+                <input
+                  className="form-control"
+                  id="inputNewPassword"
+                  type="password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                ></input>
               </div>
               <div className="form-group">
                 <label htmlFor="inputRepeatNewPassword">
@@ -23,6 +73,9 @@ const PasswordSettingsComponent = () => {
                 <input
                   className="form-control"
                   id="inputRepeatNewPassword"
+                  type="password"
+                  value={repeatNewPassword}
+                  onChange={(e) => setRepeatNewPassword(e.target.value)}
                 ></input>
               </div>
             </div>
@@ -31,6 +84,8 @@ const PasswordSettingsComponent = () => {
             Save Changes
           </button>
         </form>
+        {errors}
+        {message}
       </div>
     </div>
   );
