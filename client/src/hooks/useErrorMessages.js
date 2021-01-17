24b0f5b6d10386f870ec9ceb4129146383dbaa4f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-17 23:23:16 +0200

    created custom hook that takes errors and returns messages in jsx form

---
+++ client/src/hooks/useErrorMessages.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const useErrorMessages = ({ errors }) => {
+  const [errorMessages, setErrorMessages] = useState(errors);
+
+  useEffect(() => {
+    if (errors) {
+      return setErrorMessages(
+        <div className="alert alert-danger">
+          <ul className="my-0">
+            {errors.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }, [errors]);
+
+  return [errorMessages, setErrorMessages];
+};
+
+export default useErrorMessages;
