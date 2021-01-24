46cfda7b84ec1316177073e5170c6fecf63e2ffa
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-24 23:08:52 +0200

    refactor App to use redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Dashboard from './components/Dashboard';
 import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
-import axiosInstance from './api/buildClient';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SettingsPage from './components/SettingsPage';
 import ActivateAccount from './components/ActivateAccount';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const App = () => {
-  const [userData, setUserData] = useState(null);
-
-  const getUser = async () => {
-    try {
-      const response = await axiosInstance.get('/api/user/currentuser', {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-      });
-
-      if (response) {
-        setUserData(response.data.user);
-      }
-    } catch (err) {
-      setUserData(null);
-    }
-  };
-
-  const onUserChange = () => {
-    getUser();
-  };
-  const onUserNotLoggedIn = () => {
-    setUserData(null);
-  };
-
-  useEffect(() => {
-    if (
-      localStorage.getItem('accessToken') &&
-      localStorage.getItem('refreshToken')
-    ) {
-      getUser();
-    }
-  }, []);
-
   return (
-    <Router>
-      <Header
-        user={userData}
-        onUserNotLoggedIn={onUserNotLoggedIn}
-        onUserCheckLoggedIn={onUserChange}
-      />
-      <div className="container">
-        <Switch>
-          <Route path="/" exact component={() => <Home user={userData} />} />
-
-          <Route
-            path="/login"
-            component={() => (
-              <Login user={userData} onUserChange={onUserChange} />
-            )}
-          />
-
-          <Route
-            path="/register"
-            component={() => <Register user={userData} />}
-          />
-
-          <Route
-            path="/dashboard"
-            component={() => (
-              <Dashboard
-                user={userData}
-                onUserNotLoggedIn={onUserNotLoggedIn}
-              />
-            )}
-          />
-          <Route
-            path="/settings"
-            component={() => <SettingsPage user={userData} />}
-          />
-
-          <Route
-            path="/activate/:id"
-            component={() => <ActivateAccount user={userData} />}
-          />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={() => <Home />} />
+            <Route path="/login" component={() => <Login />} />
+            <Route path="/register" component={() => <Register />} />
+            <Route path="/dashboard" component={() => <Dashboard />} />
+            <Route path="/settings" component={() => <SettingsPage />} />
+            <Route path="/activate/:id" component={() => <ActivateAccount />} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
