8824265c6fda51b884f3a29acb57609d9bf23b06
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-31 20:31:21 +0200

    redux-unde setup

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,5 +2,10 @@ import { combineReducers } from 'redux';
 import valueReducer from './valueReducer';
 import userReducer from './userReducer';
 import errorsReducer from './errorsReducer';
+import undoable from 'redux-undo';
 
-export default combineReducers({ userReducer, errorsReducer, valueReducer });
+export default combineReducers({
+  userReducer: undoable(userReducer, { limit: 5 }),
+  errorsReducer,
+  valueReducer,
+});
