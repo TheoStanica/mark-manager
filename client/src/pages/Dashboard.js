0d5fb37741520d1c6204e26e07e26ca51d5cb02d
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-31 20:25:22 +0200

    move & refactor Dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
 import { getUserInfo } from '../redux/actions/userActions';
 
 const Dashboard = () => {
-  const user = useSelector((state) => state.userReducer);
+  useSelector((state) => state.userReducer.present);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUserInfo());
   }, [dispatch]);
-
-  if (!user.accessToken && !user.refreshToken) {
-    return <Redirect to="/login" />;
-  }
 
   return (
     <div>
