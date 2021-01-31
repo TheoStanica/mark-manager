4e54b567a5eef6101b3aa6843b42a545e5aa07de
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-31 20:12:14 +0200

    added PersistGate, added PrivateRoute component to redirect users if they are authenticated or not

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import Header from './components/Header';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SettingsPage from './components/SettingsPage';
-import ActivateAccount from './components/ActivateAccount';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import SettingsPage from './pages/SettingsPage';
+import ActivateAccount from './pages/ActivateAccount';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Header />
-        <div className="container">
-          <Switch>
-            <Route path="/" exact component={() => <Home />} />
-            <Route path="/login" component={() => <Login />} />
-            <Route path="/register" component={() => <Register />} />
-            <Route path="/dashboard" component={() => <Dashboard />} />
-            <Route path="/settings" component={() => <SettingsPage />} />
-            <Route path="/activate/:id" component={() => <ActivateAccount />} />
-          </Switch>
-        </div>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route path="/" exact component={() => <Home />} />
+              <Route
+                path="/activate/:id"
+                component={() => <ActivateAccount />}
+              />
+              <PrivateRoute path="/register" redirectIfLoggedIn={true}>
+                <Register />
+              </PrivateRoute>
+              <PrivateRoute path="/login" redirectIfLoggedIn={true}>
+                <Login />
+              </PrivateRoute>
+
+              <PrivateRoute path="/dashboard">
+                <Dashboard />
+              </PrivateRoute>
+              <PrivateRoute path="/settings">
+                <SettingsPage />
+              </PrivateRoute>
+            </Switch>
+          </div>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 };
