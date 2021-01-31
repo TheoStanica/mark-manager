d01960858b366d2bfe4eed166343017766846089
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-01-31 20:08:48 +0200

    refactor axios instance

--- client/src/api/buildClient.js
+++ client/src/api/buildClient.js
@@ -1,39 +1,54 @@
 import axios from 'axios';
-import { setUserTokens } from '../redux/actions/userActions';
+import { logoutUser } from '../redux/actions/userActions';
 import { store } from '../redux/store';
+import { isLoggedin } from '../services/isLoggedIn';
+
+const setAuthHeaderValue = () => {
+  return `Bearer ${localStorage.getItem('accessToken')}`;
+};
+const setToken = (tokenName, value) => {
+  return localStorage.setItem(tokenName, value);
+};
+const clearTokens = () => {
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+};
 
 const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'Application/json',
-    Authorization: `Bearer ${store.getState().userReducer.accessToken}`,
+    Authorization: setAuthHeaderValue(),
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = setAuthHeaderValue();
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
     if (error.config && error.response && error.response.status === 401) {
       try {
-        const response = await axiosInstance.post('api/auth/token', {
-          refreshToken: store.getState().userReducer.refreshToken,
-        });
-        if (response.status === 200) {
-          store.dispatch(
-            setUserTokens({
-              accessToken: response.data.accessToken,
-              refreshToken: response.data.refreshToken,
-            })
-          );
-          axiosInstance.defaults.headers.Authorization = `Bearer ${
-            store.getState().userReducer.accessToken
-          }`;
-          error.config.headers.Authorization = `Bearer ${
-            store.getState().userReducer.accessToken
-          }`;
+        let response;
+        if (isLoggedin()) {
+          response = await axiosInstance.post('api/auth/token', {
+            refreshToken: localStorage.getItem('refreshToken'),
+          });
         }
-        return axios.request(error.config);
+        if (response && response.status === 200) {
+          setToken('accessToken', response.data.accessToken);
+          setToken('refreshToken', response.data.refreshToken);
+          error.config.headers.Authorization = setAuthHeaderValue();
+          return axios.request(error.config);
+        }
       } catch (err) {
-        localStorage.clear();
+        clearTokens();
+        await store.dispatch(logoutUser());
         return Promise.reject(err);
       }
     }
