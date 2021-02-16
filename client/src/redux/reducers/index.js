b2713e7cd7ceea8b714a4338e26eb503119e30f5
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-02-16 16:08:35 +0200

    set up redux sync between tabs

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,9 +3,14 @@ import valueReducer from './valueReducer';
 import userReducer from './userReducer';
 import errorsReducer from './errorsReducer';
 import undoable from 'redux-undo';
+import twitterReducer from './twitterReducer';
+import { withReduxStateSync } from 'redux-state-sync';
 
-export default combineReducers({
-  userReducer: undoable(userReducer, { limit: 5 }),
-  errorsReducer,
-  valueReducer,
-});
+export default withReduxStateSync(
+  combineReducers({
+    userReducer: undoable(userReducer, { limit: 5 }),
+    errorsReducer,
+    valueReducer,
+    twitterReducer,
+  })
+);
