14808611a442b970d7ec5dac70261873deb30c10
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-02-16 16:22:13 +0200

    added ConnectTwitterButton component ( and temporary button to get user's twitter profile data)

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,14 +2,23 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserInfo } from '../redux/actions/userActions';
+import { getTwitterProfileInfoData } from '../redux/actions/twitterActions';
+import DisplayUserMessages from '../components/DisplayUserMessages';
+import ConnectTwitterButton from '../components/ConnectTwitterButton';
+import { isTwitterConnected } from '../services/isTwitterConnected';
 
 const Dashboard = () => {
   useSelector((state) => state.userReducer.present);
+  useSelector((state) => state.twitterReducer.isConnected);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUserInfo());
   }, [dispatch]);
+
+  const getTwitterInfo = () => {
+    dispatch(getTwitterProfileInfoData());
+  };
 
   return (
     <div>
@@ -17,6 +26,11 @@ const Dashboard = () => {
         Settings
       </Link>
       <Link to="/twitter/connect">Connect To Twitter</Link>
+      <div className="btn btn-primary" onClick={getTwitterInfo}>
+        GET USER TWITTER DETAILS
+      </div>
+      {!isTwitterConnected() && <ConnectTwitterButton />}
+      <DisplayUserMessages />
     </div>
   );
 };
