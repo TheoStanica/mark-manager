66704f25448c885b2d18de61a5d89443db889c81
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-02-16 23:06:54 +0200

    Updates

--- common/build/events/reset-password-event.d.ts
+++ common/build/events/reset-password-event.d.ts
@@ -2,6 +2,7 @@ import { Subjects } from './subjects';
 export interface ResetPasswordEvent {
     subject: Subjects.ResetPassword;
     data: {
+        email: string;
         resetToken: string;
     };
 }
