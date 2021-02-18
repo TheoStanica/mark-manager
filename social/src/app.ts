7f58aa33a4f91026abadf663c5534e9074ae32ea
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-02-18 23:28:06 +0200

    use route

--- social/src/app.ts
+++ social/src/app.ts
@@ -3,11 +3,13 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import { errorHandler, NotFoundError } from '@tcosmin/common';
 import { twitterCredentialsRouter } from './routes/twitter/verify-credentials';
+import { twitterTimelineRouter } from './routes/twitter/home-timeline';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 
+app.use(twitterTimelineRouter);
 app.use(twitterCredentialsRouter);
 
 app.all('*', async (req, res) => {
