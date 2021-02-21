cb4d3305d48835d1bb612646a8ce62a4ca313365
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-02-21 18:58:00 +0200

    Use Navigation component, move wrapper div inside providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import SettingsPage from './pages/SettingsPage';
-import ActivateAccount from './pages/ActivateAccount';
-import PrivateRoute from './components/PrivateRoute';
-import TwitterConnect from './pages/TwitterConnect';
-import ResetPassword from './pages/ResetPassword';
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
-    <div className="min-vh-100 d-flex flex-column">
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div className="min-vh-100 d-flex flex-column">
             <Header />
             <div className="container flex-grow-1">
               <Switch>
-                <Route path="/" exact component={() => <Home />} />
-                <Route
-                  path="/activate/:id"
-                  component={() => <ActivateAccount />}
-                />
-                <PrivateRoute path="/register" redirectIfLoggedIn={true}>
-                  <Register />
-                </PrivateRoute>
-                <PrivateRoute path="/login" redirectIfLoggedIn={true}>
-                  <Login />
-                </PrivateRoute>
-                <PrivateRoute path="/password/reset" redirectIfLoggedIn={true}>
-                  <ResetPassword />
-                </PrivateRoute>
-
-                <PrivateRoute path="/dashboard">
-                  <Dashboard />
-                </PrivateRoute>
-                <PrivateRoute path="/settings">
-                  <SettingsPage />
-                </PrivateRoute>
-                <PrivateRoute path="/twitter/connect">
-                  <TwitterConnect />
-                </PrivateRoute>
+                <Navigation />
               </Switch>
             </div>
-          </Router>
-        </PersistGate>
-      </Provider>
-    </div>
+          </div>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 };
 
