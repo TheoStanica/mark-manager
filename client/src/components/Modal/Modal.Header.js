ef8ff6f6a7e72e6a62b561ba3924d91bcb1de1ee
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-03 11:04:49 +0200

    created modal header component

---
+++ client/src/components/Modal/Modal.Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CardHeader from '../Card/Card.Header';
+
+const ModalHeader = ({ children }) => {
+  return <CardHeader>{children}</CardHeader>;
+};
+
+export default ModalHeader;
