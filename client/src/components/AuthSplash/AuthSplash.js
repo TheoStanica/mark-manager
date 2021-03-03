d21d33fc6adf028038b130bcc803ce3dfa8ed1b2
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-03 10:40:00 +0200

    Created AuthSplash component to display Login and Register components inside of it

---
+++ client/src/components/AuthSplash/AuthSplash.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './AuthSplash.css';
+
+const AuthSplash = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div className="authsplash">
+        {children}
+        <div className="bg">
+          <img src="./AuthSplashBg.svg" alt="background splash" />
+          <div className="bg-text">Project Mark</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthSplash;
