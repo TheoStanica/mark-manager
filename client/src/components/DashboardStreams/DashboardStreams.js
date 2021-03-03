91a2ba0c4e1598cc75a2301c09cde13beb932021
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-03 10:57:37 +0200

    created DashboardStreams component

---
+++ client/src/components/DashboardStreams/DashboardStreams.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardStreams = () => {
+  return (
+    <div className="dashboardStreams ml-2 mr-2 mt-2 mb-2">
+      Hello there this is the streams component
+    </div>
+  );
+};
+
+export default DashboardStreams;
