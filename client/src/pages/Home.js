1236dd6f09045d05429f1284d7e4d8a572f1e48f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-03 10:20:43 +0200

    redirect to dashboard if user is logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import { decreaseValue, increaseValue } from '../redux/actions/valueActions';
+import { isLoggedin } from '../services/isLoggedIn';
 
 const Home = () => {
   const value = useSelector((state) => state.valueReducer.value);
   const dispatch = useDispatch();
 
-  return (
-    <div className="ui container">
+  return isLoggedin() ? (
+    <Redirect to="/dashboard" />
+  ) : (
+    <div className="home" style={{ width: '100%' }}>
+      <Header className="normal" />
       <div>value ={value}</div>
       <button
         className="btn btn-primary me-2"
