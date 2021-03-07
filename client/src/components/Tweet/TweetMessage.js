6c46218545d27b1eff1c6cafbb81c8e2046ab230
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-07 23:52:08 +0200

    component to replace text links with working links and remove media (images) links from text message

---
+++ client/src/components/Tweet/TweetMessage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import twitter from 'twitter-text';
+
+const TweetMessage = ({ tweet }) => {
+  let { text, entities } = tweet;
+
+  // replace links
+  text = twitter.autoLinkWithJSON(text, entities);
+
+  // remove Image Link
+  if (entities && entities.media) {
+    entities.media.forEach((element) => {
+      text = text.replace(element.display_url, '');
+    });
+  }
+
+  return (
+    <p
+      dangerouslySetInnerHTML={{
+        __html: text,
+      }}
+    ></p>
+  );
+};
+
+export default TweetMessage;
