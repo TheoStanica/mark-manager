c77f15d3e0094393027daa6e37c34bdbfe1a688b
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-07 23:22:36 +0200

    show button that displays connected accounts

--- client/src/components/DashboardMenu/DashboardMenu.js
+++ client/src/components/DashboardMenu/DashboardMenu.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import ClearButton from '../ClearButton/ClearButton';
-import ConnectTwitterButton from '../ConnectTwitterButton';
 import './DashboardMenu.css';
+import ConnectedAccounts from '../ConnectedAccounts/ConnectedAccounts';
 
 const DashboardMenu = () => {
-  const { isconnected } = useSelector((state) => state.twitterReducer);
-
   return (
     <div className="dashboard-menu">
-      <ClearButton>Add Stream</ClearButton>
-      {isconnected ? <ConnectTwitterButton /> : null}
+      <ConnectedAccounts />
     </div>
   );
 };
