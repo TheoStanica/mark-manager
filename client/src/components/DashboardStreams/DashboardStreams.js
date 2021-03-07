ebf9a53ae11591851c4460145a945f06d4902560
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-07 23:23:46 +0200

    load timelines

--- client/src/components/DashboardStreams/DashboardStreams.js
+++ client/src/components/DashboardStreams/DashboardStreams.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import TwitterHomeTimeline from '../TwitterHomeTimeline';
+import './DashboardStreams.css';
 
 const DashboardStreams = () => {
+  const { streams } = useSelector((state) => state.twitterReducer);
+
   return (
-    <div className="dashboardStreams ml-2 mr-2 mt-2 mb-2">
-      Hello there this is the streams component
+    <div className="dashboardStreams ">
+      <div className="streamswrapper pt-05 pb-05">
+        {streams &&
+          streams.map((stream, idx) => {
+            switch (stream) {
+              case 'home':
+                return <TwitterHomeTimeline key={idx} />;
+              default:
+                return null;
+            }
+          })}
+      </div>
     </div>
   );
 };
