2c0bb17d93100df2835ecdc7518550be8676e8d3
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-09 15:33:23 +0200

    export assets

---
+++ client/src/assets/Pictures/index.js
@@ -0,0 +1,15 @@
+import AuthSplash from './AuthSplashBg.svg';
+import CreateIcon from './CreateIcon.svg';
+import Loading from './Loading.svg';
+import MarkLogo from './MarkLogo.svg';
+import RefreshIcon from './Refresh.svg';
+import StreamsIcon from './StreamsIcon.svg';
+
+export const Pictures = {
+  AuthSplash,
+  CreateIcon,
+  Loading,
+  MarkLogo,
+  RefreshIcon,
+  StreamsIcon,
+};
