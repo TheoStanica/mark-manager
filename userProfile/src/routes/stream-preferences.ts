2c82c1d0004ea8e4bcac119dcddcb4124596dbe0
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-18 13:48:42 +0200

    created route to update user's stream preferences

---
+++ userProfile/src/routes/stream-preferences.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import { BadRequestError, requireAuth, validateRequest } from '@tcosmin/common';
+import { body } from 'express-validator';
+import { UserProfileController } from '../controllers/userprofile-controller';
+import { UserDoc } from '../models/userprofile';
+
+const router = express.Router();
+
+router.post(
+  '/api/user/streampreferences',
+  requireAuth,
+  [
+    body('stream_preferences')
+      .exists()
+      .withMessage('stream_preferences is required.'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { stream_preferences } = req.body as UserDoc;
+    const user = await UserProfileController.updateStreamPreferences(
+      req.currentUser!.userId,
+      stream_preferences
+    );
+
+    if (!user) {
+      throw new BadRequestError(
+        'Something went wrong. Please try again later!'
+      );
+    }
+
+    res.send({ streams: user.stream_preferences });
+  }
+);
+
+export { router as streamPreferencesRouter };
