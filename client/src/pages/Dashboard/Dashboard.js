93c9275a80b323336ebddbb7eb9c9dec397fcf99
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-21 00:49:35 +0200

    clean up Dashboard page a little

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -5,24 +5,16 @@ import VerticalMenu from '../../components/VerticalMenu/VerticalMenu';
 import './Dashboard.css';
 import DashboardMenu from '../../components/DashboardMenu/DashboardMenu';
 import DashboardStreams from '../../components/DashboardStreams/DashboardStreams';
-import {
-  getTwitterHomeTimeline,
-  getTwitterProfileInfoData,
-} from '../../redux/actions/twitterActions';
+import { getTwitterProfileInfoData } from '../../redux/actions/twitterActions';
 
 const Dashboard = () => {
-  const { isConnected } = useSelector((state) => state.twitterReducer);
   useSelector((state) => state.userReducer.present);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const request = async () => {
-      await dispatch(getUserInfo());
-      await dispatch(getTwitterProfileInfoData());
-      if (isConnected) dispatch(getTwitterHomeTimeline());
-    };
-    request();
-  }, [dispatch, isConnected]);
+    dispatch(getTwitterProfileInfoData());
+    dispatch(getUserInfo());
+  }, [dispatch]);
 
   return (
     <>
