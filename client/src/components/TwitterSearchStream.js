4d8da81a9cae81975116de9c6fdf82dd1c4044aa
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-21 14:01:45 +0200

    add Twitter search Stream

---
+++ client/src/components/TwitterSearchStream.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  loadTweetSearchStream,
+  removeStream,
+} from '../redux/actions/twitterActions';
+import Timeline from './Timeline/Timeline';
+import TimelineBody from './Timeline/TimelineBody';
+import TimelineHeader from './Timeline/TimelineHeader';
+import TweetCard from './Tweet/TweetCard';
+import Loading from './Loading/Loading';
+
+const TwitterSearchStream = ({ stream, provided }) => {
+  const { screenName } = useSelector((state) => state.twitterReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTweetSearchStream({ id: stream.id, search: stream.search }));
+  }, [dispatch, stream.id, stream.search]);
+
+  return (
+    <Timeline>
+      <TimelineHeader
+        type={`Search ${stream.search}`}
+        account={screenName}
+        className="pb-05"
+        onRefresh={() =>
+          dispatch(
+            loadTweetSearchStream({ id: stream.id, search: stream.search })
+          )
+        }
+        onRemove={() => {
+          dispatch(removeStream({ id: stream.id }));
+        }}
+        {...provided.dragHandleProps}
+      ></TimelineHeader>
+      <TimelineBody>
+        {stream.isLoading ? (
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Loading />
+          </div>
+        ) : (
+          stream.tweets &&
+          stream.tweets.map((tweet, idx) => {
+            return <TweetCard tweet={tweet} key={idx} />;
+          })
+        )}
+      </TimelineBody>
+    </Timeline>
+  );
+};
+
+export default TwitterSearchStream;
