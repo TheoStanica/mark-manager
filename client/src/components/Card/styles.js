494f41ef5b06e0c59de0b828cb18efd3f92ffdb8
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-23 21:20:15 +0200

    created styles for Card using styled components

---
+++ client/src/components/Card/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  background: white;
+  box-shadow: 0 0 0.875rem 0 rgb(33 37 41 / 5%);
+`;
+
+export const StyledCardBody = styled.div`
+  padding: 1rem;
+`;
+export const StyledCardHeader = styled.div`
+  font-size: 1.5rem;
+  padding: 0.7rem 1rem;
+  white-space: initial;
+`;
+export const StyledCardFooter = styled.div`
+  padding: 0.7rem 1rem;
+  margin-left: auto;
+`;
