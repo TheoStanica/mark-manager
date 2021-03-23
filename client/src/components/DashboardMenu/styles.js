054c43dc8d92fe4052dca97fdf6547a6074b61ea
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-23 21:53:37 +0200

    created styled-component styles

---
+++ client/src/components/DashboardMenu/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledDashboardMenu = styled.div`
+  background-color: #e3b5a4;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
+`;
