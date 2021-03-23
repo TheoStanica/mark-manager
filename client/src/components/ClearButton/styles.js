eb142e98fe691bba409097fdaf63346c3ec63ebc
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-23 21:43:20 +0200

    created styled-component to style the button

---
+++ client/src/components/ClearButton/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledClearButton = styled.button`
+  background: none;
+  outline: none;
+  border: 2px solid #2c0064;
+  border-radius: 20px;
+  padding: 0.3rem 1.5rem;
+  margin: 0 0.2rem 0 0;
+  font-size: 1rem;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+`;
