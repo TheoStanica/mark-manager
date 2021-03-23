468749699596377955c773e5d49637f9203ac25b
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-23 21:20:58 +0200

    refactor card components to use styled components

--- client/src/components/Card/CardFooter.js
+++ client/src/components/Card/CardFooter.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { StyledCardFooter } from './styles';
 
 const CardFooter = ({ children }) => {
-  return <div className="card-footer">{children}</div>;
+  return <StyledCardFooter>{children}</StyledCardFooter>;
 };
 
 export default CardFooter;
