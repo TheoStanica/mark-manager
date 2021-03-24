c7ed3dca1363e043ae3c3763846a65143264859e
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-24 10:44:49 +0200

    removed Icon css file, created new styles using styled-components and refactored the component to use them

---
+++ client/src/components/Icon/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledIcon = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    cursor: pointer;
+  }
+`;
