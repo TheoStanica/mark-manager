e33edc784156834ac3409c034dd8378eda60e2dc
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-24 20:11:09 +0200

    removed page css file, created new styles with styled-components and refactored the page to use them

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,22 +3,22 @@ import { Link, Redirect } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Header from '../../components/Header/Header';
 import { isLoggedin } from '../../services/isLoggedIn';
-import './Home.css';
+import { StyledHome, StyledHomeWrapper } from './styles';
 
 const Home = () => {
   return isLoggedin() ? (
     <Redirect to="/dashboard" />
   ) : (
-    <div className="home" style={{ width: '100%' }}>
-      <Header className="normal" />
-      <div className=" home-wrapper d-flex flex-column justify-content-center align-items-center">
+    <StyledHome>
+      <Header normal={true} />
+      <StyledHomeWrapper>
         <h1>Welcome to Project Mark!</h1>
         <Button style={{ marginBottom: '1rem', marginTop: '1rem' }}>
           <Link to="/login">Login</Link>
         </Button>
         <div className="soon">More coming soon...</div>
-      </div>
-    </div>
+      </StyledHomeWrapper>
+    </StyledHome>
   );
 };
 
---
+++ client/src/pages/Home/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledHome = styled.div`
+  width: 100%;
+  h1 {
+    font-size: 3rem;
+    padding: 0 1rem;
+  }
+  button {
+    align-self: center;
+  }
+  .soon {
+    font-size: 0.7rem;
+  }
+`;
+
+export const StyledHomeWrapper = styled.div`
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
