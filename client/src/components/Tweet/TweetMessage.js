9904aea92910fa7e63e6027d6e61cfd9b6312972
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-25 11:15:21 +0200

    renamed full_text to text

--- client/src/components/Tweet/TweetMessage.js
+++ client/src/components/Tweet/TweetMessage.js
@@ -3,22 +3,22 @@ import twitter from 'twitter-text';
 import { StyledMessage } from './styles';
 
 const TweetMessage = ({ tweet }) => {
-  let { full_text, entities } = tweet;
+  let { full_text: text, entities } = tweet;
 
   // replace links
-  full_text = twitter.autoLinkWithJSON(full_text, entities);
+  text = twitter.autoLinkWithJSON(text, entities);
 
   // remove Image Link
   if (entities && entities.media) {
     entities.media.forEach((element) => {
-      full_text = full_text.replace(element.display_url, '');
+      text = text.replace(element.display_url, '');
     });
   }
 
   return (
     <StyledMessage
       dangerouslySetInnerHTML={{
-        __html: full_text,
+        __html: text,
       }}
     ></StyledMessage>
   );
