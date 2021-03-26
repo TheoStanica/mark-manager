057e05ad942cfc733333fc83a3847126128bed76
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-27 00:18:50 +0200

    added offset and position props and refactored component to render either an icon with a tooltip or not

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ToolTip from '../ToolTip/ToolTip';
 import { StyledIcon } from './styles';
 
-const Icon = ({ size, style, icon, tooltip, onClick }) => {
-  return (
-    <ToolTip text={tooltip}>
+const Icon = ({ size, style, icon, tooltip, position, offset, onClick }) => {
+  const renderIcon = (style, icon, onClick) => {
+    return (
       <StyledIcon
         style={{ height: size + 'px', width: size + 'px', ...style }}
         onClick={onClick}
       >
         <img src={icon} alt={icon} />
       </StyledIcon>
+    );
+  };
+
+  return tooltip ? (
+    <ToolTip text={tooltip} position={position} offset={offset}>
+      {renderIcon(style, icon, onClick)}
     </ToolTip>
+  ) : (
+    renderIcon(style, icon, onClick)
   );
 };
 
