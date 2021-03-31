20c0eac61dab2b18f395605e9d0a0467824446be
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-03-31 15:44:28 +0300

    refactor to use stream component

--- client/src/components/TwitterHomeTimeline.js
+++ client/src/components/TwitterHomeTimeline.js
@@ -1,77 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   loadHomeTimelineStream,
-  removeStream,
   loadMoreHomeTimelineStream,
 } from '../redux/actions/twitterActions';
-import Loading from './Loading/Loading';
-import Timeline from './Timeline/Timeline';
-import TimelineBody from './Timeline/TimelineBody';
-import TimelineHeader from './Timeline/TimelineHeader';
-import TweetCard from './Tweet/TweetCard';
-import InfiniteScroll from 'react-infinite-scroller';
+import Stream from './Stream';
 
-const TwitterHomeTimeline = React.memo(({ id, provided }) => {
-  const { screenName } = useSelector((state) => state.twitterReducer);
-  const stream = useSelector((state) => state.twitterReducer.streamsById[id]);
+const TwitterHomeTimeline = React.memo(({ stream, provided }) => {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadHomeTimelineStream({ id: stream.id }));
-  }, [dispatch, stream.id]);
-
-  const renderTweets = () => {
-    return (
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={() =>
-          dispatch(
-            loadMoreHomeTimelineStream({
-              id: stream.id,
-              maxId: stream.metadata.max_id,
-            })
-          )
-        }
-        hasMore={true}
-        useWindow={false}
-        threshold={500}
-      >
-        {stream.tweets.map((tweet, idx) => {
-          return <TweetCard tweet={tweet} key={idx} />;
-        })}
-      </InfiniteScroll>
-    );
-  };
-
   return (
-    <Timeline>
-      <TimelineHeader
-        type="Home"
-        account={screenName}
-        onRefresh={() => dispatch(loadHomeTimelineStream({ id: stream.id }))}
-        onRemove={() => {
-          dispatch(removeStream({ id: stream.id }));
-        }}
-        {...provided.dragHandleProps}
-      ></TimelineHeader>
-      <TimelineBody>
-        {stream.isLoading ? (
-          <div
-            style={{
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Loading />
-          </div>
-        ) : stream.tweets ? (
-          renderTweets()
-        ) : null}
-      </TimelineBody>
-    </Timeline>
+    <Stream
+      id={stream.id}
+      provided={provided}
+      type="Home"
+      onLoad={useCallback(() => {
+        dispatch(loadHomeTimelineStream({ id: stream.id }));
+      }, [dispatch, stream.id])}
+      onLoadMore={() =>
+        dispatch(
+          loadMoreHomeTimelineStream({
+            id: stream.id,
+            maxId: stream.metadata.max_id,
+          })
+        )
+      }
+    />
   );
 });
 
