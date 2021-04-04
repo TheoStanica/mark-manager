650aa4e29670441e0ab2cfd85402719984b1d51f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-05 01:17:54 +0300

    refactor function as getUserTwitterAccountTokens will always return tokens

--- social/src/services/getTwitterAccountTokens.ts
+++ social/src/services/getTwitterAccountTokens.ts
@@ -1,16 +1,11 @@
-import { BadRequestError } from '@tcosmin/common';
 import { UserController } from '../controllers/user-controller';
 
 export const getTwitterAccountTokens = async (
   userId: string,
   twitterUserId: string
 ) => {
-  const data = await UserController.getUserTwitterAccountTokens(
+  return await UserController.getUserTwitterAccountTokens(
     userId,
     twitterUserId
   );
-  if (!data) {
-    throw new BadRequestError('Please provide a valid userId');
-  }
-  return data;
 };
