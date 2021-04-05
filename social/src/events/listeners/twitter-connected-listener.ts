3b5cddee968313243173f4b4ad0f5452873decf5
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-05 12:22:02 +0300

    refactor listener to ack the message only after a successful token insert in db

--- social/src/events/listeners/twitter-connected-listener.ts
+++ social/src/events/listeners/twitter-connected-listener.ts
@@ -1,4 +1,9 @@
-import { Listener, Subjects, TwitterConnectedEvent } from '@tcosmin/common';
+import {
+  DatabaseConnectionError,
+  Listener,
+  Subjects,
+  TwitterConnectedEvent,
+} from '@tcosmin/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { UserController } from '../../controllers/user-controller';
@@ -17,14 +22,17 @@ export class TwitterConnectedListener extends Listener<TwitterConnectedEvent> {
       twitterUserId,
     } = data;
 
-    await UserController.addTwitterTokens({
-      userID: id,
-      oauthAccessToken,
-      oauthAccessTokenSecret,
-      twitterScreenName,
-      twitterUserId,
-    });
-
-    msg.ack();
+    try {
+      const added = await UserController.addTwitterTokens({
+        userID: id,
+        oauthAccessToken,
+        oauthAccessTokenSecret,
+        twitterScreenName,
+        twitterUserId,
+      });
+      if (added) msg.ack();
+    } catch (err) {
+      throw new DatabaseConnectionError();
+    }
   }
 }
