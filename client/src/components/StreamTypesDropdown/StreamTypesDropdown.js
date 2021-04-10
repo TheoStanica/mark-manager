3c9f9c0f5640c2191e11764f94097448fe1b16bd
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-10 22:12:54 +0300

    created dropdown to select available types of streams

---
+++ client/src/components/StreamTypesDropdown/StreamTypesDropdown.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'home_timeline', label: 'Home Timeline' },
+  { value: 'search', label: 'Search' },
+];
+
+const StreamTypesDropdown = ({ onSelected, reset }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    if (reset) {
+      setSelectedOption(null);
+    }
+  }, [reset]);
+
+  const handleChange = (selected) => {
+    setSelectedOption(selected);
+    onSelected(selected.value);
+  };
+
+  return (
+    <Select
+      value={selectedOption}
+      options={options}
+      onChange={handleChange}
+      isSearchable={true}
+    />
+  );
+};
+
+export default StreamTypesDropdown;
