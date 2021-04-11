26d8f9f8778e7a8cbe21bd9efe7cd16191a0cc42
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-11 21:14:30 +0300

    add filter streams component into the menu

--- client/src/components/DashboardMenu/DashboardMenu.js
+++ client/src/components/DashboardMenu/DashboardMenu.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ConnectedAccounts from '../ConnectedAccounts/ConnectedAccounts';
 import AddStream from '../AddStream';
 import { StyledDashboardMenu } from './styles';
+import FilterStreams from '../FilterStreams/FilterStreams';
 
 const DashboardMenu = () => {
   return (
     <StyledDashboardMenu>
       <ConnectedAccounts />
       <AddStream />
+      <FilterStreams />
     </StyledDashboardMenu>
   );
 };
