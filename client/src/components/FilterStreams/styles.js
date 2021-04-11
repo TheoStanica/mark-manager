b472967bfe4fee3f0053a08da894f08222a7f311
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-11 23:57:44 +0300

    add box shadow so the component can be seen easily

--- client/src/components/FilterStreams/styles.js
+++ client/src/components/FilterStreams/styles.js
@@ -5,6 +5,7 @@ export const StyledFilterDiv = styled.div`
   padding: 0.5rem;
   border-radius: 5px;
   background: white;
+  box-shadow: 0 0 0.875rem 0 rgb(33 37 41 / 20%);
 `;
 
 export const StyledFilterButton = styled.button`
