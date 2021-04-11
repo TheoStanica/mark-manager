41f322c4b29c3bd3c4cf893bde9c16a4e08fd072
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-11 23:52:35 +0300

    add header to show "Dashboard" on the page

--- client/src/components/DashboardMenu/DashboardMenu.js
+++ client/src/components/DashboardMenu/DashboardMenu.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ConnectedAccounts from '../ConnectedAccounts/ConnectedAccounts';
 import AddStream from '../AddStream';
-import { StyledDashboardMenu } from './styles';
+import { StyledDashboardMenu, StyledHeader } from './styles';
 import FilterStreams from '../FilterStreams/FilterStreams';
 
 const DashboardMenu = () => {
   return (
     <StyledDashboardMenu>
+      <StyledHeader>Dashboard</StyledHeader>
       <ConnectedAccounts />
       <AddStream />
       <FilterStreams />
