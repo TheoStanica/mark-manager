63b9032ed104705666fff9bde4a66bba58dfcc50
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-11 23:51:40 +0300

    add white-space, add backgorund, remove border

--- client/src/components/ClearButton/styles.js
+++ client/src/components/ClearButton/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledClearButton = styled.button`
-  background: none;
+  white-space: nowrap;
+  background: #068abc;
+  color: white;
   outline: none;
-  border: 2px solid #2c0064;
+  border: none;
   border-radius: 20px;
   padding: 0.3rem 1.5rem;
   margin: 0 0.2rem 0 0;
