ef187bd707bebaca7182c094e0207feee7547f69
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-11 23:57:17 +0300

    changed paddings

--- client/src/components/DashboardStreams/styles.js
+++ client/src/components/DashboardStreams/styles.js
@@ -4,17 +4,16 @@ export const StyledDashboardStreams = styled.div`
   height: calc(100vh - 60px);
   width: calc(100vw - 80px);
   white-space: nowrap;
+  padding-top: 0.3rem;
 `;
 
 export const StyledStreamsWrapper = styled.div`
   overflow: scroll;
   height: 100%;
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
 `;
 
 export const StyledStreamContainer = styled.li`
-  margin-left: 0.5rem;
+  margin-left: 0.3rem;
 `;
 
 export const StyledStreamsList = styled.ul`
