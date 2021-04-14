f0b3d3ba57ed9eaf48d0c8340fd24919b7161244
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-14 21:16:57 +0300

    refactor component to use the new Retweet svg

--- client/src/components/Tweet/RetweetHeader.js
+++ client/src/components/Tweet/RetweetHeader.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Assets } from '../../assets';
 import Icon from '../Icon/Icon';
 import { StyledRetweetHeader } from './styles';
+import Retweet from '../../assets/Pictures/Retweet';
 
 const RetweetHeader = ({ tweet }) => {
   return (
     <StyledRetweetHeader>
-      <Icon
-        icon={Assets.Pictures.Retweet}
-        size={18}
-        style={{ cursor: 'initial', marginRight: 5 }}
-      />
+      <Icon size={18} style={{ cursor: 'initial', marginRight: 5 }}>
+        <Retweet color="#000" />
+      </Icon>
       <p>
         <b>{tweet.user.name}</b> retweeted:
       </p>
