c6fa080edf5dff23021f7659ed034b9fe5aed3b6
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-16 20:39:46 +0300

    refactor to use CustomPopper component

--- client/src/components/FilterStreams/FilterStreams.js
+++ client/src/components/FilterStreams/FilterStreams.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { usePopper } from 'react-popper';
-import ReactDOM from 'react-dom';
 import { StyledFilterDiv } from './styles';
 import { useDispatch, useSelector } from 'react-redux';
 import FilterButton from './FilterButton';
 import { filterAccounts } from '../../redux/actions/twitterActions';
 import Icon from '../Icon/Icon';
 import Filter from '../../assets/Pictures/Filter';
+import CustomPopper from '../CustomPopper/CustomPopper';
 
 const FilterStreams = () => {
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
   const { twitterAccounts, twitterFilteredAccounts } = useSelector(
     (state) => state.twitterReducer
   );
@@ -19,17 +16,6 @@ const FilterStreams = () => {
   );
   const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
-  const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: 'top-start',
-    modifiers: [
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 10],
-        },
-      },
-    ],
-  });
 
   useEffect(() => {
     setSelectedAccountFilter(twitterFilteredAccounts);
@@ -52,27 +38,23 @@ const FilterStreams = () => {
 
   return (
     <>
-      <Icon
-        ref={setReferenceElement}
-        onClick={() => setIsOpen(!isOpen)}
+      <CustomPopper
+        open={isOpen}
         style={{ marginRight: '.5rem' }}
-      >
-        <Filter size={35} color={'#333'} />
-      </Icon>
-      {isOpen &&
-        ReactDOM.createPortal(
-          <StyledFilterDiv
-            ref={setPopperElement}
-            style={styles.popper}
-            {...attributes.popper}
-          >
+        placement="bottom-end"
+        popper={
+          <StyledFilterDiv>
             <div style={{ marginBottom: '0.5rem', fontSize: '1.5rem' }}>
               Filter streams
             </div>
             {renderAccountsFilters()}
-          </StyledFilterDiv>,
-          document.body
-        )}
+          </StyledFilterDiv>
+        }
+      >
+        <Icon onClick={() => setIsOpen(!isOpen)}>
+          <Filter size={35} color={'#333'} />
+        </Icon>
+      </CustomPopper>
     </>
   );
 };
