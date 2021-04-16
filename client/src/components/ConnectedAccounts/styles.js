3fc911b4db740cf368486a1d081dc0be1e7a8925
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-04-16 15:58:41 +0300

    created new style

--- client/src/components/ConnectedAccounts/styles.js
+++ client/src/components/ConnectedAccounts/styles.js
@@ -28,3 +28,9 @@ export const StyledConnect = styled.div`
   padding: 1rem 0;
   border-top: 1px solid rgba(0, 0, 0, 0.08);
 `;
+
+export const StyledCenteredDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
