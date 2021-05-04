a0ddb5af5593d85854db8af94b6f29a6717d0768
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-04 21:10:42 +0300

    change color for scrollbars on dark mode

--- client/src/components/Timeline/styles.js
+++ client/src/components/Timeline/styles.js
@@ -36,4 +36,16 @@ export const StyledTimelineBody = styled.div`
   &::-webkit-scrollbar {
     width: 0.3rem;
   }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${(props) =>
+      props.theme.pref === 'dark'
+        ? 'rgba(255, 255, 255, 0.3)'
+        : 'rgba(0, 0, 0, 0.2)'};
+  }
+  &:hover::-webkit-scrollbar-thumb {
+    background-color: ${(props) =>
+      props.theme.pref === 'dark'
+        ? 'rgba(255, 255, 255, 0.7)'
+        : 'rgba(0, 0, 0, 0.5)'};
+  }
 `;
