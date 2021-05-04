ad48d4330c2e660ab0803ba014f5582431c45942
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-04 17:43:09 +0300

    render icons and colors with different colors if a specific tweet is liked or retweeted

--- client/src/components/Tweet/TweetFooter.js
+++ client/src/components/Tweet/TweetFooter.js
@@ -5,8 +5,12 @@ import { StyledTweetFooterContainer } from './styles';
 import Retweet from '../../assets/Pictures/Retweet';
 import Likes from '../../assets/Pictures/Likes';
 import { withTheme } from 'styled-components';
+import FilledLikes from '../../assets/Pictures/FilledLikes';
 
 const TweetFooter = ({ tweet, theme }) => {
+  const isLiked = tweet.favorited;
+  const isRetweeted = tweet.retweeted;
+
   const formatValue = (count) => {
     return Intl.NumberFormat('en-US', {
       maximumFractionDigits: 1,
@@ -24,15 +28,46 @@ const TweetFooter = ({ tweet, theme }) => {
           position={'top-start'}
           offset={[0, 5]}
         >
-          <Retweet color={theme.pref === 'dark' ? 'white' : 'black'} />
+          {isRetweeted ? (
+            <Retweet color="#17bf63" />
+          ) : (
+            <Retweet color={theme.pref === 'dark' ? 'white' : 'black'} />
+          )}
         </Icon>
-        <p style={{ marginLeft: 5, marginRight: 20, fontWeight: 'bold' }}>
+        <p
+          style={{
+            marginLeft: 5,
+            marginRight: 20,
+            fontWeight: 'bold',
+            fontSize: '.9rem',
+            color: isRetweeted
+              ? '#17bf63'
+              : theme.pref === 'dark'
+              ? 'white'
+              : 'black',
+          }}
+        >
           {formatValue(tweet.retweet_count)}
         </p>
         <Icon size={18} tooltip={'Like'} position={'top-start'} offset={[0, 5]}>
-          <Likes color={theme.pref === 'dark' ? 'white' : 'black'} />
+          {isLiked ? (
+            <FilledLikes color="#e0245e" />
+          ) : (
+            <Likes color={theme.pref === 'dark' ? 'white' : 'black'} />
+          )}
         </Icon>
-        <p style={{ marginLeft: 5, fontWeight: 'bold' }}>
+        <p
+          style={{
+            marginLeft: 5,
+            fontWeight: 'bold',
+            color: isLiked
+              ? '#e0245e'
+              : theme.pref === 'dark'
+              ? 'white'
+              : 'black',
+            fontSize: '.9rem',
+          }}
+        >
           {formatValue(tweet.favorite_count)}
         </p>
       </StyledTweetFooterContainer>
