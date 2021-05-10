47b4cdff6a62b274041504a0a9b75f05dbcf6990
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-10 14:00:15 +0300

    route to get replies of a tweet

---
+++ social/src/routes/twitter/comments.ts
@@ -0,0 +1,78 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@tcosmin/common';
+import twit, { Twitter } from 'twit';
+import { query } from 'express-validator';
+import { fetchTwitterAccountTokens } from '../../services/getTwitterAccountTokens';
+import { handleTwitterErrors } from '../../services/handleTwitterErrors';
+
+const router = express.Router();
+const consumerKey = process.env.TWITTER_CONSUMER_KEY!;
+const consumerSecret = process.env.TWITTER_CONSUMER_SECRET!;
+
+interface TwitterResponse {
+  data: {
+    statuses: any[];
+    search_metadata: any;
+  };
+}
+
+router.get(
+  '/api/social/twitter/search/tweets/comments',
+  requireAuth,
+  [
+    query('twitterUserId')
+      .notEmpty()
+      .isNumeric()
+      .withMessage('Please provide a valid Twitter user ID'),
+    query('maxId')
+      .optional()
+      .isNumeric()
+      .withMessage('maxId must be a numeric value'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const {
+      search,
+      maxId,
+      twitterUserId,
+      repliesTo,
+      tweetRepliedTo,
+      sinceId,
+    } = req.query;
+    const {
+      oauthAccessToken,
+      oauthAccessTokenSecret,
+    } = await fetchTwitterAccountTokens(
+      req.currentUser!.userId,
+      String(twitterUserId)
+    );
+    const twitterClient = new twit({
+      consumer_key: consumerKey,
+      consumer_secret: consumerSecret,
+      access_token: oauthAccessToken,
+      access_token_secret: oauthAccessTokenSecret,
+    });
+    try {
+      const tweets = ((await twitterClient.get('search/tweets', {
+        q: `to:${repliesTo}`,
+        tweet_mode: 'extended',
+        since_id: sinceId ? String(sinceId) : undefined,
+        max_id: maxId ? String(maxId) : undefined,
+        count: 100,
+        in_reply_to_status_id: String(tweetRepliedTo),
+        result_type: 'recent',
+      })) as unknown) as TwitterResponse;
+      let resArray: any[] = [];
+      tweets.data.statuses.forEach((tweet) => {
+        if (tweet.in_reply_to_status_id_str === tweetRepliedTo) {
+          resArray.push(tweet);
+        }
+      });
+      res.send(resArray);
+    } catch (err) {
+      handleTwitterErrors(err, String(twitterUserId));
+    }
+  }
+);
+
+export { router as twitterCommentsRouter };
