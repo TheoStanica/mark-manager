b35f1abd36ebfcbc021728d243ae8272af51700c
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-14 18:39:50 +0300

    add validation for fields

--- social/src/routes/twitter/replies.ts
+++ social/src/routes/twitter/replies.ts
@@ -19,11 +19,14 @@ interface TwitterResponse {
 router.get(
   '/api/social/twitter/search/tweets/comments',
   requireAuth,
-  [
+  [ 
+    query('repliesToScreenName').notEmpty().withMessage("Please provide a Twitter Screen Name"),
+    query('inReplyToStatusId').notEmpty().isNumeric().withMessage("Please provide a valid ID"),
     query('twitterUserId')
       .notEmpty()
       .isNumeric()
       .withMessage('Please provide a valid Twitter user ID'),
+    query("sinceId").notEmpty().isNumeric().withMessage("Please provide a valid ID"),
     query('maxId')
       .optional()
       .isNumeric()
