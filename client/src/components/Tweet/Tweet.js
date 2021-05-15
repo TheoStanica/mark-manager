3a9f568fd1dfe61e14aa1fc1c1d178f80cf5c78e
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-15 23:03:50 +0300

    created a new component to display more details (replies) for a specific tweet

---
+++ client/src/components/Tweet/Tweet.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  StyledTweetDetailsClose,
+  StyledTweetDetailsWrapper,
+  TweetWrapper,
+} from './styles';
+
+const Tweet = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const changeIsVisible = (e) => {
+    e.stopPropagation();
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <>
+      <TweetWrapper onClick={changeIsVisible}>{children}</TweetWrapper>
+      {isVisible
+        ? ReactDOM.createPortal(
+            <>
+              <StyledTweetDetailsClose onClick={changeIsVisible}>
+                X
+              </StyledTweetDetailsClose>
+              <StyledTweetDetailsWrapper>{children}</StyledTweetDetailsWrapper>
+            </>,
+            document.body
+          )
+        : null}
+    </>
+  );
+};
+
+export default Tweet;
