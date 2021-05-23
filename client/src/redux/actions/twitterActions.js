0d0304f13ea09ae600367e18944523adcb78304e
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-24 01:41:06 +0300

    created action to fetch replies

--- client/src/redux/actions/twitterActions.js
+++ client/src/redux/actions/twitterActions.js
@@ -484,3 +484,40 @@ export const retweetTweet = ({
     dispatch(handleError({ error: err }));
   }
 };
+
+export const fetchTweetReplies = ({
+  twitterUserId,
+  repliesToScreenName,
+  inReplyToStatusId,
+  sinceId,
+}) => async (dispatch) => {
+  await dispatch(setRepliesLoadingStatus({ isLoading: true }));
+  const response = await axiosInstance.get(
+    TwitterEndpoints.fetchTweetReplies({
+      twitterUserId,
+      repliesToScreenName,
+      inReplyToStatusId,
+      sinceId,
+    })
+  );
+  if (response.data.length === 0) {
+    await dispatch(setRepliesLoadingStatus({ isLoading: false }));
+    return;
+  }
+  const repliesArray = response.data.map((tweet) => tweet.id_str);
+  const tweetsObject = response.data.reduce(
+    (obj, tweet) => ({ ...obj, [tweet.id_str]: { ...tweet } }),
+    {}
+  );
+
+  await dispatch({
+    type: TWITTER_SET_TWEET_REPLIES,
+    payload: {
+      replies: tweetsObject,
+      repliesArray: repliesArray,
+      maxId: response.data[response.data.length - 1].id_str,
+      moreReplies: response.data.length === 30 ? true : false,
+    },
+  });
+  await dispatch(setRepliesLoadingStatus({ isLoading: false }));
+};
