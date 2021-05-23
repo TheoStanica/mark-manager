d2f6f8d9dc5fe318d88987154b5a7077a6de0e12
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-05-24 01:31:08 +0300

    added endpoint to fetch replies

--- client/src/services/twitterApiEndpoints.js
+++ client/src/services/twitterApiEndpoints.js
@@ -9,6 +9,16 @@ export const TwitterEndpoints = {
     `/api/social/twitter/statuses/home_timeline?maxId=${maxId}&twitterUserId=${twitterUserId}`,
   fetchTwitterAccountDataEndpoint: ({ twitterUserId }) =>
     `/api/social/twitter/user?twitterUserId=${twitterUserId}`,
+  fetchTweetReplies: ({
+    twitterUserId,
+    repliesToScreenName,
+    inReplyToStatusId,
+    sinceId,
+    maxId,
+  }) =>
+    maxId
+      ? `/api/social/twitter/search/tweets/comments?twitterUserId=${twitterUserId}&repliesToScreenName=${repliesToScreenName}&inReplyToStatusId=${inReplyToStatusId}&sinceId=${sinceId}&maxId=${maxId}}`
+      : `/api/social/twitter/search/tweets/comments?twitterUserId=${twitterUserId}&repliesToScreenName=${repliesToScreenName}&inReplyToStatusId=${inReplyToStatusId}&sinceId=${sinceId}`,
   fetchTwitterAccountsEndpoint: '/api/social/twitter/accounts',
   connectToTwitterEndpoint: '/api/auth/twitter/connect',
   tweetMessageEndpoint: '/api/social/twitter/statuses/update',
