9e9fb69a188151eabc0942e4927b7af6eb2e8b04
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-10-31 17:52:48 +0200

    small change

--- userProfile/src/routes/uploadimage.ts
+++ userProfile/src/routes/uploadimage.ts
@@ -1,19 +1,18 @@
 import express, { Request, Response } from 'express';
 import { BadRequestError, requireAuth } from '@tcosmin/common';
-import { AWSImageUploader } from '../services/aws-image-upload';
+import { imageUploadService } from '../services/imageUploadService';
 
 const router = express.Router();
 
 router.post(
   '/api/user/uploadimage',
   requireAuth,
-  AWSImageUploader.upload.single('image'),
+  imageUploadService.upload.single('image'),
   async (req: Request, res: Response) => {
     if (!req.file) {
       throw new BadRequestError('No file provided');
     }
 
-    // TODO figure out this
     //@ts-ignore
     res.send({ imageUrl: req.file.location });
   }
