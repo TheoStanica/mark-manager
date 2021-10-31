1dceefc3d7ca4358a6c1bee67d0034a41fc7d4be
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-10-31 12:53:08 +0200

    added scheduler component and added a new page for app

---
+++ client/src/pages/PlannerPage/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledSettings = styled.div`
+  transition: 0.2s ease-in-out;
+  flex-grow: 1;
+  background: ${(props) =>
+    props.theme.pref === 'dark' ? '#30353A' : '#e5e8ed'};
+`;
+export const StyledHeader = styled.h1`
+  transition: 0.2s ease-in-out;
+  font-size: 2.4rem;
+  margin-bottom: 15px;
+  font-weight: normal;
+  color: ${(props) => (props.theme.pref === 'dark' ? 'white' : 'black')};
+`;
+
+export const StyledSettingsRow = styled.div`
+  display: flex;
+  @media only screen and (max-width: 900px) {
+    flex-direction: column;
+  }
+`;
