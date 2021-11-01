fa297f83a6d38273ba466ce184707e1d0b842136
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-01 23:09:58 +0200

    signin logic

--- auth/src/repositories/userRepository.ts
+++ auth/src/repositories/userRepository.ts
@@ -1,8 +1,9 @@
-import { BadRequestError } from '@tcosmin/common';
+import { AccountNotActivatedError, BadRequestError } from '@tcosmin/common';
 import { Service } from 'typedi';
 import { User, UserModel } from '../models/users';
 import { UserCredentialsDto } from '../utils/dtos/userCredentialsDto';
 import crypto from 'crypto';
+import { Password } from '../services/password';
 
 @Service()
 export class UserRepository {
@@ -21,6 +22,30 @@ export class UserRepository {
     const user = this.User.build(userCredentialsDto);
     user.confirmationToken = crypto.randomBytes(20).toString('hex');
     return await user.save();
+  }
+
+  async validateCredentials(userCredentialsDto: UserCredentialsDto) {
+    const { email, password } = userCredentialsDto;
+
+    const user = await this.fetchUser(email);
+
+    if (!user) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(user.password, password);
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    if (!user.confirmed) {
+      throw new AccountNotActivatedError(user.id);
+    }
+
+    return user;
+  }
+
+  private async fetchUser(email: string) {
+    return this.User.findOne({ email });
   }
 
   private async isEmailInUse(email: string) {
