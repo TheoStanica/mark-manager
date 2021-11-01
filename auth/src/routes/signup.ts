81be92b9f0dd88facfd59e5a7531edc24d0a8f03
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-01 15:34:30 +0200

    refactored route to use AuthService

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,19 +1,9 @@
-import {
-  BadRequestError,
-  DatabaseConnectionError,
-  validateRequest,
-} from '@tcosmin/common';
+import { validateRequest } from '@tcosmin/common';
 import express, { Request, Response } from 'express';
-import { User } from '../models/users';
 import { body } from 'express-validator';
-import { redisWrapper } from '../redis-wrapper';
-import { RedisService } from '../services/redis-service';
-import { TokenService } from '../services/token-service';
-import { UserController } from '../controllers/userController';
-import crypto from 'crypto';
-import { UserCreatedPublisher } from '../events/publishers/user-created-publisher';
-import { natsWrapper } from '../nats-wrapper';
-import { SendActivationEmailPublisher } from '../events/publishers/send-activation-email-publisher';
+import { CreateUserDto } from '../utils/dtos/createUserDto';
+import Container from 'typedi';
+import { AuthService } from '../services/authService';
 
 const router = express.Router();
 
@@ -28,51 +18,12 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const redisService = new RedisService(redisWrapper.client);
+    const createUserDto = req.body as CreateUserDto;
 
-    const { email, password } = req.body;
+    const authService = Container.get(AuthService);
+    await authService.createUser(createUserDto);
 
-    if (await UserController.checkIfUserExists(email)) {
-      throw new BadRequestError('Email in use');
-    }
-
-    const user = UserController.createUser({ email, password });
-
-    // const accessToken = TokenService.generateAccessToken({
-    //   userId: user.id,
-    //   email: user.email,
-    //   role: 'free',
-    // });
-
-    // const refreshToken = TokenService.generateRefreshToken({
-    //   userId: user.id,
-    //   email: user.email,
-    //   role: 'free',
-    // });
-
-    // whitelist tokens
-    // redisService.whitelistRefreshTokens({
-    //   userId: user.id,
-    //   accessToken,
-    //   refreshToken,
-    // });
-
-    // save UID_RT : AT in Redis(RTTL)
-    // save AT : RT in Redis (RTTL)
-
-    await new UserCreatedPublisher(natsWrapper.client).publish({
-      id: user.id!,
-      email: user.email,
-    });
-
-    await new SendActivationEmailPublisher(natsWrapper.client).publish({
-      email: user.email,
-      activationToken: user.confirmationToken,
-    });
-
-    // TODO change this to send only "User Created, please check your Email to confirm your account" or something similar...
-    // after Mailer microservice is up and running.
-    res.status(201).send({ user });
+    res.sendStatus(201);
   }
 );
 
