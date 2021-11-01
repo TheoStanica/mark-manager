c478bb40c896659eab136511f02c27e0179f54cd
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-01 22:24:09 +0200

    refactored route

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,40 +1,18 @@
-import {
-  ForbiddenError,
-  NotAuthorizedError,
-  requireAuth,
-} from '@tcosmin/common';
+import { requireAuth } from '@tcosmin/common';
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
-import { redisWrapper } from '../redis-wrapper';
-import jwt from 'jsonwebtoken';
-import { RedisService } from '../services/redis-service';
-import { AccessTokenRevokedPublisher } from '../events/publishers/access-token-revoked-publisher';
-import { natsWrapper } from '../nats-wrapper';
+import Container from 'typedi';
+import { AuthService } from '../services/authService';
 
 const router = express.Router();
 
 router.post('/signout', requireAuth, async (req: Request, res: Response) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    console.log('Token not found');
-    throw new ForbiddenError();
-  }
+  const authService = Container.get(AuthService);
+  await authService.logoutUser(authHeader);
 
-  const redisService = new RedisService(redisWrapper.client);
-
-  if (await redisService.isRevoked(token)) {
-    throw new NotAuthorizedError();
-  }
-
-  await redisService.logoutUser(token);
-
-  await new AccessTokenRevokedPublisher(natsWrapper.client).publish({
-    token: token,
-  });
-
-  res.send({});
+  res.sendStatus(204);
 });
 
 export { router as signoutRouter };
