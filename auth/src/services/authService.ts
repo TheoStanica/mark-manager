a6281763b9f98acaf94f39ca3715ee9e250cafb5
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-01 22:22:24 +0200

    logout method

--- auth/src/services/authService.ts
+++ auth/src/services/authService.ts
@@ -1,16 +1,22 @@
+import { ForbiddenError, NotAuthorizedError } from '@tcosmin/common';
 import { Service } from 'typedi';
+import { AccessTokenRevokedPublisher } from '../events/publishers/access-token-revoked-publisher';
 import { SendActivationEmailPublisher } from '../events/publishers/send-activation-email-publisher';
 import { UserCreatedPublisher } from '../events/publishers/user-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
 import { UserRepository } from '../repositories/userRepository';
-import { CreateUserDto } from '../utils/dtos/createUserDto';
+import { UserCredentialsDto } from '../utils/dtos/userCredentialsDto';
+import { RedisService } from './redis-service';
 
 @Service()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly redisService: RedisService
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto) {
-    const user = await this.userRepository.createUser(createUserDto);
+  async createUser(userCredentialsDto: UserCredentialsDto) {
+    const user = await this.userRepository.createUser(userCredentialsDto);
 
     await new UserCreatedPublisher(natsWrapper.client).publish({
       id: user.id,
@@ -20,6 +26,22 @@ export class AuthService {
     await new SendActivationEmailPublisher(natsWrapper.client).publish({
       email: user.email,
       activationToken: user.confirmationToken,
+    });
+  }
+
+  async logoutUser(authHeader: string | undefined) {
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      throw new ForbiddenError();
+    }
+    if (await this.redisService.isRevoked(token)) {
+      throw new NotAuthorizedError();
+    }
+
+    await this.redisService.logoutUser(token);
+    await new AccessTokenRevokedPublisher(natsWrapper.client).publish({
+      token,
     });
   }
 }
