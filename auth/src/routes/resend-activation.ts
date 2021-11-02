c7872d3a0e1146a7dd106dbad5533d86f67739b9
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-02 16:03:02 +0200

    route refactoring

--- auth/src/routes/resend-activation.ts
+++ auth/src/routes/resend-activation.ts
@@ -1,53 +1,23 @@
-import {
-  AccountAlreadyActivatedError,
-  BadRequestError,
-  validateRequest,
-} from '@tcosmin/common';
+import { validateRequest } from '@tcosmin/common';
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-import { UserController } from '../controllers/userController';
-import { SendActivationEmailPublisher } from '../events/publishers/send-activation-email-publisher';
-import { natsWrapper } from '../nats-wrapper';
+import Container from 'typedi';
+import { AuthService } from '../services/authService';
+import { ActivationRequestDto } from '../utils/dtos/activationRequestDto';
+import { activationRequestValidation } from '../utils/validation/activationRequestValidation';
 
 const router = express.Router();
 
 router.post(
   '/activation/resend',
-  [
-    body('userId')
-      .isLength({ max: 24, min: 24 })
-      .withMessage('UserID is not valid')
-      .isHexadecimal()
-      .withMessage('UserID is not valid'),
-  ],
+  activationRequestValidation,
   validateRequest,
   async (req: Request, res: Response) => {
-    //get userId from req.params
-    const { userId } = req.body;
+    const activationRequestDto = req.body as ActivationRequestDto;
 
-    // check if there is a user with that userID
-    // if there isnt throw BadRequestError
-    const user = await UserController.findUserWithId(userId);
-    if (!user) {
-      throw new BadRequestError('UserId is not valid');
-    }
-    if (user.confirmed) {
-      throw new AccountAlreadyActivatedError();
-    }
+    const authService = Container.get(AuthService);
+    await authService.resendActivationRequest(activationRequestDto);
 
-    // else, generate a new activation Token, update expiration time, and send event to message broker
-    const updatedUser = await UserController.generateNewActivationDetails(
-      user.id!
-    );
-
-    await new SendActivationEmailPublisher(natsWrapper.client).publish({
-      email: updatedUser!.email,
-      activationToken: updatedUser!.confirmationToken,
-    });
-
-    // TODO change status to 204 and remove updatedUser
-
-    res.status(200).send(updatedUser);
+    res.sendStatus(204);
   }
 );
 
