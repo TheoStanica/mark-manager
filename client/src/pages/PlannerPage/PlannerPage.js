4ac5ddef997e5dd67cf43a68c62b251083f96fe7
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-07 20:19:08 +0200

    planner page

--- client/src/pages/PlannerPage/PlannerPage.js
+++ client/src/pages/PlannerPage/PlannerPage.js
@@ -1,74 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import VerticalMenu from '../../components/VerticalMenu/VerticalMenu';
-import { getUserInfo } from '../../redux/actions/userActions';
 import { StyledSettings } from './styles';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-  Scheduler,
-  DayView,
-  Appointments,
-  MonthView,
-  AppointmentTooltip,
-  AppointmentForm,
-  WeekView,
-  Toolbar,
-  ViewSwitcher,
-  DateNavigator,
-  TodayButton,
-} from '@devexpress/dx-react-scheduler-material-ui';
-import Paper from '@mui/material/Paper';
+
+import { fetchScheduledTweets } from '../../redux/actions/scheduledTweetsActions';
+import Planner from '../../components/Planner/Planner';
 
 const PlannerPage = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-
   useSelector((state) => state.userReducer.present);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getUserInfo());
+    // dispatch(fetchScheduledTweets());
   }, [dispatch]);
-
-  const data = [
-    {
-      title: 'Website Re-Design Plan',
-      startDate: '2021-10-29T09:45',
-      endDate: '2021-10-29T19:45',
-    },
-    {
-      title: 'Website Re-Design Plan',
-      startDate: new Date(2021, 10, 29, 9, 30),
-      endDate: new Date(2021, 10, 29, 13, 30),
-    },
-  ];
 
   return (
     <>
       <VerticalMenu />
-      {/* <StyledSettings> */}
-
-      <StyledSettings>
-        <Scheduler data={data}>
-          <ViewState
-            currentDate={currentDate}
-            defaultCurrentViewName="Month"
-            onCurrentDateChange={(date) => setCurrentDate(date)}
-          />
-          <MonthView />
-          {/* <MonthView.Layout setScrollingStrategy="changeVerticalScroll" /> */}
-          <WeekView startDayHour={7} endDayHour={20} />
-          <DayView startDayHour={7} endDayHour={20} />
-          <Toolbar />
-          <DateNavigator />
-          <TodayButton />
-          <ViewSwitcher />
-          <Appointments />
-          <AppointmentTooltip showCloseButton showOpenButton />
-          <AppointmentForm readOnly />
-        </Scheduler>
+      <StyledSettings style={{ color: 'red', backgroundColor: 'white' }}>
+        <Planner />
       </StyledSettings>
-      {/* </StyledSettings> */}
     </>
   );
 };
