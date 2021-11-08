afe863455fda57e7c0b4349fb16af01cb4e76767
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-08 23:05:49 +0200

    no more used

--- client/src/components/Planner/validation/appointmentValidation.js
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-export const appointmentValidation = yup.object({
-  twitterUserId: yup.string().required('Account is required'),
-  scheduledAt: yup.date('asas').required('Schedule date is required'),
-  message: yup.string('asgasg').required(),
-});
