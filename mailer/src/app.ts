dadb9ba7b26bec78dc6947d56082e59e70c7a68c
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-11-20 16:20:40 +0200

    mailer production build container, health check route

--- mailer/src/app.ts
+++ mailer/src/app.ts
@@ -7,7 +7,9 @@ const app = express();
 app.set('trust proxy', true);
 app.use(json());
 
-//
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- mailer/src/index.ts
+++ mailer/src/index.ts
@@ -39,6 +39,7 @@ const start = async () => {
     new PasswordResetListener(natsWrapper.client).listen();
   } catch (err) {
     console.log(err);
+    process.exit();
   }
 
   app.listen(3000, () => {
