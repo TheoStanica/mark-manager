8288018f03e6019f0c85d3421ef4dfc7839e90c6
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-12-02 22:38:18 +0200

    added routes for stress tests and moved them to internal routes folder

--- mailer/src/app.ts
+++ mailer/src/app.ts
@@ -2,14 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import { errorHandler, NotFoundError } from '@tcosmin/common';
+import { internalRouter } from './routes/internal';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200);
-});
+app.use('/', internalRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
