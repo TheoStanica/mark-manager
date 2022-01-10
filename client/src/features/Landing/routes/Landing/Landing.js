a7113fb9a2ffb2448a526a69a60cc9741f66deb1
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-10 14:52:14 +0200

    finished landing page

--- client/src/features/Landing/routes/Landing/Landing.js
+++ client/src/features/Landing/routes/Landing/Landing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Pictures } from '../../../../assets/Pictures';
 import PublicNavigation from '../../../PublicNavigation/components/PublicNavigation';
+import GradientButton from '../../../../core/components/GradientButton/GradientButton';
 import {
   Box,
   Container,
@@ -9,8 +11,6 @@ import {
   useMediaQuery,
   useTheme,
 } from '@mui/material';
-import GradientButton from '../../../../core/components/GradientButton/GradientButton';
-import { Pictures } from '../../../../assets/Pictures';
 
 const Landing = () => {
   const theme = useTheme();
@@ -20,11 +20,12 @@ const Landing = () => {
   return (
     <>
       <PublicNavigation
-        appBarProps={{ color: 'transparent', sx: { position: 'absolute' } }}
+        appBarProps={navigationAppBarStyle}
         linkStyle={{ color: theme.palette.primary.contrastText }}
       />
       <CssBaseline />
-      <Box container sx={gradientBackgroundStyle} component="section">
+      <Box sx={gradientBackgroundStyle(isLowerSizeScreen)}></Box>
+      <Box container sx={sectionStyle} component="section">
         <Container sx={contentContainerStyle} maxWidth="xl">
           <Box sx={copyCTAStyle}>
             <Box>
@@ -56,7 +57,7 @@ const Landing = () => {
               Register now
             </GradientButton>
           </Box>
-          <Box sx={copCTAImageStyle(isLowerSizeScreen)}>
+          <Box sx={copyCTAImageStyle(isLowerSizeScreen)}>
             <img
               style={{ width: '100%' }}
               src={Pictures.EngageNew}
@@ -65,7 +66,49 @@ const Landing = () => {
           </Box>
         </Container>
       </Box>
-      {/* <Box container sx={gradientBackgroundStyle} component="section"></Box> */}
+      <Box container sx={sectionStyle} component="section">
+        <Container sx={contentContainerStyle} maxWidth="xl">
+          <Box sx={{ ...copyCTAImageStyle(isLowerSizeScreen), width: '38%' }}>
+            <img
+              style={{ width: '100%' }}
+              src={Pictures.OpinionsNew}
+              alt="Find what your customers really think"
+            />
+          </Box>
+          <Box sx={{ ...copyCTAStyle, pl: isLowerSizeScreen ? 0 : 8 }}>
+            <Typography {...copyH1Props} sx={inlineStyle(theme)}>
+              Find what your customers really think
+            </Typography>
+            <Typography {...copyH3Props} sx={{ ...inlineStyle(theme), mt: 3 }}>
+              Keep an eye on the latest social conversations, trends and brand
+              mentions. Quickly respond to comments for all your connected
+              accounts
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+      <Box container sx={sectionStyle} component="section">
+        <Container sx={contentContainerStyle} maxWidth="xl">
+          <Box sx={copyCTAStyle}>
+            <Typography {...copyH1Props} sx={inlineStyle(theme)}>
+              Connect all your social networks and never miss an event
+            </Typography>
+            <GradientButton
+              sx={{ mt: 4 }}
+              onClick={() => history.push('/register')}
+            >
+              Register now
+            </GradientButton>
+          </Box>
+          <Box sx={copyCTAImageStyle(isLowerSizeScreen)}>
+            <img
+              style={{ width: '100%' }}
+              src={Pictures.ConnectAccountsNew}
+              alt="Find what your customers really think"
+            />
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 };
@@ -77,46 +120,35 @@ const inlineStyle = (theme, emphasized) => ({
   fontWeight: emphasized ? 600 : 'inherit',
 });
 
-const gradient1 =
-  'radial-gradient(at 4% 2%, hsla(0,0%,0%,1) 0, transparent 64%), radial-gradient(at 99% 1%, hsla(158,48%,58%,1) 0, transparent 46%), radial-gradient(at 66% 100%, hsla(238,100%,75%,1) 0, transparent 59%), radial-gradient(at 99% 71%, hsla(238,37%,53%,1) 0, transparent 43%), radial-gradient(at 63% 11%, hsla(238,37%,53%,1) 0, transparent 82%)';
-
-const gradient2 =
-  'radial-gradient(at 97% 78%, hsla(238,37%,53%,1) 0, transparent 43%), radial-gradient(at 20% 23%, hsla(223,39%,8%,1) 0, transparent 73%), radial-gradient(at 59% 100%, hsla(238,100%,75%,1) 0, transparent 59%), radial-gradient(at 98% 3%, hsla(158,48%,58%,1) 0, transparent 46%), radial-gradient(at 89% 14%, hsla(238,37%,53%,1) 0, transparent 82%)';
-
-const gradient3 =
-  'radial-gradient(at 90% 20%, hsla(228,97%,71%,1) 0, transparent 52%), radial-gradient(at 59% 85%, hsla(177,78%,67%,1) 0, transparent 57%), radial-gradient(at 82% 42%, hsla(124,66%,72%,1) 0, transparent 49%), radial-gradient(at 44% 6%, hsla(239,65%,75%,1) 0, transparent 47%), radial-gradient(at 33% 30%, hsla(255,98%,66%,1) 0, transparent 55%);';
-
-const gradient4 =
+const gradient =
   'radial-gradient(at 100% 100%, hsla(238,32%,15%,1) 0, transparent 40%), radial-gradient(at 18% 23%, hsla(240,30%,4%,1) 0, transparent 100%), radial-gradient(at 94% 4%, hsla(158,48%,58%,1) 0, transparent 46%), radial-gradient(at 56% 91%, hsla(238,100%,75%,1) 0, transparent 59%), radial-gradient(at 94% 44%, hsla(238,37%,53%,1) 0, transparent 58%), radial-gradient(at 59% 1%, hsla(238,37%,53%,1) 0, transparent 82%);';
 
-const gradientBackgroundStyle = {
+const gradientBackgroundStyle = (isLowerSizeScreen) => ({
+  position: 'fixed',
+  minHeight: '100vh',
+  width: '100%',
+  top: 0,
+  left: 0,
+  zIndex: -1,
   backgroundColor: '#0b0f19',
-  // backgroundImage:
-  //   'radial-gradient(at 97% 78%, hsla(238,37%,53%,1) 0, transparent 43%), radial-gradient(at 20% 23%, hsla(223,39%,8%,1) 0, transparent 73%), radial-gradient(at 59% 100%, hsla(238,100%,75%,1) 0, transparent 59%), radial-gradient(at 98% 3%, hsla(158,48%,58%,1) 0, transparent 46%), radial-gradient(at 89% 14%, hsla(238,37%,53%,1) 0, transparent 82%)',
-  // // backgroundColor: '#000000',
-  // backgroundImage: 'linear-gradient(90deg, #ee6352, purple, #ee6352)',
-
-  // backgroundImage:
-  //   'radial-gradient(at 4% 2%, hsla(0,0%,0%,1) 0, transparent 64%), radial-gradient(at 99% 1%, hsla(158,48%,58%,1) 0, transparent 46%), radial-gradient(at 66% 100%, hsla(238,100%,75%,1) 0, transparent 59%), radial-gradient(at 99% 71%, hsla(238,37%,53%,1) 0, transparent 43%), radial-gradient(at 63% 11%, hsla(238,37%,53%,1) 0, transparent 82%)',
-
-  height: 'calc(100vh - 64px)',
-  height: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-
-  animation: 'bg-animation 25s ease-in-out infinite',
-
-  // backgroundImage: 'linear-gradient(red,blue)',
-  background: gradient4,
-  backgroundPosition: 'bottom',
+  background: gradient,
+  animation: isLowerSizeScreen
+    ? 'none'
+    : 'bg-animation 25s ease-in-out infinite',
 
   '@keyframes bg-animation': {
     '0%': { backgroundSize: '100% 100%' },
     '50%': { backgroundSize: '170% 120%', backgroundPosition: ' top left' },
     '100%': { backgroundSize: '100% 100% ' },
   },
+});
+
+const navigationAppBarStyle = {
+  color: 'transparent',
+  sx: { position: 'absolute' },
 };
+
+const sectionStyle = { display: 'flex', minHeight: '100vh' };
 
 const contentContainerStyle = {
   display: 'flex',
@@ -131,8 +163,8 @@ const copyCTAStyle = {
   alignItems: 'baseline',
 };
 
-const copCTAImageStyle = (isLowerSizeScreen) => ({
-  width: '45vw',
+const copyCTAImageStyle = (isLowerSizeScreen) => ({
+  width: '40vw',
   display: isLowerSizeScreen ? 'none' : 'flex',
 });
 
@@ -143,6 +175,10 @@ const copyH1Props = {
 const copyH2Props = {
   component: 'h2',
   variant: 'h5',
+};
+const copyH3Props = {
+  component: 'h3',
+  variant: 'body',
 };
 
 export default Landing;
