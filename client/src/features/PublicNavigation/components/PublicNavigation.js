f8aa2d4b5214b466330c05ff88d6c6cf09ac7ef6
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-10 14:51:46 +0200

    added padding for different screen sizes

--- client/src/features/PublicNavigation/components/PublicNavigation.js
+++ client/src/features/PublicNavigation/components/PublicNavigation.js
@@ -1,35 +1,60 @@
 import React from 'react';
-import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+} from '@mui/material';
 import Mark from '../../../assets/Pictures/Mark';
 import Link from '../../../core/components/Link/Link';
 
-const PublicNavigation = ({ appBarProps, toolBarProps, linkStyle }) => {
+const PublicNavigation = ({
+  appBarProps,
+  toolBarStyle,
+  toolBarProps,
+  linkStyle,
+}) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" elevation={5} {...appBarProps}>
-        <Toolbar {...toolBarProps}>
-          <Box sx={{ mr: 2 }}>
-            <Link to="/" style={{ display: 'flex' }} aria-label="Mark">
-              <Mark size={48} />
-            </Link>
-          </Box>
-          <Typography variant="h6" component="p" sx={{ flexGrow: 1 }}>
-            Mark
-          </Typography>
-          <Button color="inherit">
-            <Link to="/login" style={linkStyle}>
-              Login
-            </Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/register" style={linkStyle}>
-              Register
-            </Link>
-          </Button>
-        </Toolbar>
+        <Container maxWidth="xl">
+          <Toolbar
+            sx={{
+              ...toolBarDefaultStyle,
+              ...toolBarStyle,
+            }}
+            {...toolBarProps}
+          >
+            <Box sx={{ mr: 2 }}>
+              <Link to="/" style={{ display: 'flex' }} aria-label="Mark">
+                <Mark size={48} />
+              </Link>
+            </Box>
+            <Typography variant="h6" component="p" sx={{ flexGrow: 1 }}>
+              Mark
+            </Typography>
+            <Button color="inherit" sx={{ mr: 1 }}>
+              <Link to="/login" style={linkStyle}>
+                Login
+              </Link>
+            </Button>
+            <Button color="inherit">
+              <Link to="/register" style={linkStyle}>
+                Register
+              </Link>
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
+};
+
+const toolBarDefaultStyle = {
+  '@media (min-width: 600px)': { padding: 0 },
+  padding: 0,
 };
 
 export default PublicNavigation;
