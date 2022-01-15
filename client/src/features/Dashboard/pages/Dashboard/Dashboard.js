af3770c3b43ba447634c8a4571f73d30ceb422f5
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-15 16:37:04 +0200

    initial dashboard page

---
+++ client/src/features/Dashboard/pages/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useLogoutMutation } from '../../../../api/auth/api';
+import AppDrawer from '../../../../core/components/AppDrawer/AppDrawer';
+import useApplicationTheme from '../../../../core/hooks/useApplicationTheme';
+
+const Dashboard = () => {
+  const [logout] = useLogoutMutation();
+  const { toggleTheme } = useApplicationTheme();
+
+  return (
+    <AppDrawer title="Dashboard">
+      <button onClick={logout}>logout</button>
+
+      <button onClick={toggleTheme}>toggle theme</button>
+    </AppDrawer>
+  );
+};
+
+export default Dashboard;
