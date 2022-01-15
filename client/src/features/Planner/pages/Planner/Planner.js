046d4324a556122f1684e627a18eb22cef4f0a95
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-15 21:33:25 +0200

    generic page

---
+++ client/src/features/Planner/pages/Planner/Planner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppDrawer from '../../../../core/components/AppDrawer/AppDrawer';
+
+const Planner = () => {
+  return <AppDrawer title="Planner">Planner page</AppDrawer>;
+};
+
+export default Planner;
