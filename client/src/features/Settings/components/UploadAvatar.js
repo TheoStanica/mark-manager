bb9562c0d80c519c308bb31ef0970fae3086abe7
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-16 18:40:56 +0200

    created component that handles user's avatar and previews of uploaded images

---
+++ client/src/features/Settings/components/UploadAvatar.js
@@ -0,0 +1,42 @@
+import { Avatar, IconButton, Input } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useCurrentUserQuery } from '../../../api/user/api';
+
+const UploadAvatar = ({ onImageUploaded }) => {
+  const { data } = useCurrentUserQuery();
+  const [imageLink, setImageLink] = useState(data?.user?.profilePicture);
+
+  useEffect(() => {
+    const avatar = data?.user?.profilePicture;
+    if (avatar) {
+      setImageLink(avatar);
+    }
+  }, [data]);
+
+  const onImageUpload = (e) => {
+    if (e?.target?.files[0] && e?.target?.files[0].type === 'image/jpeg') {
+      const blob = new Blob([e.target.files[0]], { type: 'image/jpeg' });
+      setImageLink(URL.createObjectURL(blob));
+      onImageUploaded(e.target.files[0]);
+    }
+  };
+
+  return (
+    <>
+      <Input
+        accept="image/*"
+        id="contained-button-file"
+        type="file"
+        sx={{ display: 'none' }}
+        onChange={onImageUpload}
+      />
+      <label htmlFor="contained-button-file">
+        <IconButton component="span">
+          <Avatar sx={{ width: 150, height: 150, margin: 3 }} src={imageLink} />
+        </IconButton>
+      </label>
+    </>
+  );
+};
+
+export default UploadAvatar;
