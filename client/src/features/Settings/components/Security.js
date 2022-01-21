e108171ed391742702306be2fae62ff486514729
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-21 15:43:27 +0200

    refactored component - Formik Component => useFormik

--- client/src/features/Settings/components/Security.js
+++ client/src/features/Settings/components/Security.js
@@ -1,16 +1,11 @@
-import {
-  CircularProgress,
-  Container,
-  Paper,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { CircularProgress, Container, Paper, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import { useSnackbar } from 'notistack';
 import React, { useEffect } from 'react';
 import { useChangePasswordMutation } from '../../../api/auth/api';
 import DisplayError from '../../../core/components/DisplayError';
+import FormikTextField from '../../../core/components/FormikTextField';
 import GradientButton from '../../../core/components/GradientButton';
 import { changePasswordSchema } from '../validation/changePassword';
 
@@ -20,6 +15,17 @@ const Security = () => {
     { isLoading, isError, error, isSuccess },
   ] = useChangePasswordMutation();
   const { enqueueSnackbar } = useSnackbar();
+  const formik = useFormik({
+    validationSchema: changePasswordSchema,
+    initialValues: {
+      currentPassword: '',
+      newPassword: '',
+      confirmNewPassword: '',
+    },
+    onSubmit: async ({ currentPassword, newPassword }) => {
+      await changePassword({ currentPassword, newPassword });
+    },
+  });
 
   useEffect(() => {
     if (isSuccess) {
@@ -33,105 +39,45 @@ const Security = () => {
         <Typography component="h1" variant="h5" mb={2}>
           Change Password
         </Typography>
-        <Formik
-          validationSchema={changePasswordSchema}
-          initialValues={{
-            currentPassword: '',
-            newPassword: '',
-            confirmNewPassword: '',
-          }}
-          onSubmit={async ({ currentPassword, newPassword }) => {
-            await changePassword({ currentPassword, newPassword });
-          }}
-        >
-          {({
-            handleBlur,
-            values,
-            handleChange,
-            errors,
-            touched,
-            handleSubmit,
-          }) => (
-            <form>
-              <TextField
-                variant="standard"
-                margin="normal"
-                required
-                fullWidth
-                id="newPassword"
-                label="New Password"
-                type="password"
-                name="newPassword"
-                autoComplete="newPassword"
-                value={values.newPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                helperText={
-                  errors.newPassword && touched.newPassword
-                    ? errors.newPassword
-                    : null
-                }
-                error={errors.newPassword && touched.newPassword}
-              />
-              <TextField
-                variant="standard"
-                margin="normal"
-                required
-                fullWidth
-                id="confirmNewPassword"
-                label="Confirm New Password"
-                type="password"
-                name="confirmNewPassword"
-                autoComplete="confirmNewPassword"
-                value={values.confirmNewPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                helperText={
-                  errors.confirmNewPassword && touched.confirmNewPassword
-                    ? errors.confirmNewPassword
-                    : null
-                }
-                error={errors.confirmNewPassword && touched.confirmNewPassword}
-              />
-              <TextField
-                variant="standard"
-                margin="normal"
-                required
-                fullWidth
-                id="currentPassword"
-                label="Current Password"
-                type="password"
-                name="currentPassword"
-                autoComplete="currentPassword"
-                value={values.currentPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                helperText={
-                  errors.currentPassword && touched.currentPassword
-                    ? errors.currentPassword
-                    : null
-                }
-                error={errors.currentPassword && touched.currentPassword}
-              />
-              {isError && <DisplayError error={error} />}
-              <Box>
-                <GradientButton
-                  type="submit"
-                  onClick={handleSubmit}
-                  fullWidth
-                  sx={{ mt: 3, mb: 2 }}
-                  startIcon={
-                    isLoading ? (
-                      <CircularProgress color="inherit" size={20} />
-                    ) : null
-                  }
-                >
-                  Change Password
-                </GradientButton>
-              </Box>
-            </form>
-          )}
-        </Formik>
+        <form>
+          <FormikTextField
+            formik={formik}
+            id="newPassword"
+            label="New Password"
+            type="password"
+            required
+          />
+          <FormikTextField
+            formik={formik}
+            id="confirmNewPassword"
+            label="Confirm New Password"
+            type="password"
+            required
+          />
+          <FormikTextField
+            formik={formik}
+            id="currentPassword"
+            label="Current Password"
+            type="password"
+            required
+          />
+          {isError && <DisplayError error={error} />}
+          <Box>
+            <GradientButton
+              type="submit"
+              onClick={formik.handleSubmit}
+              fullWidth
+              sx={{ mt: 3, mb: 2 }}
+              startIcon={
+                isLoading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null
+              }
+            >
+              Change Password
+            </GradientButton>
+          </Box>
+        </form>
       </Paper>
     </Container>
   );
