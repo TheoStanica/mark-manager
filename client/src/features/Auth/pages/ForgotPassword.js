854d5b6567e21431fac942c8e1bf5c9a840c1f3c
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-21 15:30:47 +0200

    refactored component - Formik Component => useFormik

--- client/src/features/Auth/pages/ForgotPassword.js
+++ client/src/features/Auth/pages/ForgotPassword.js
@@ -4,13 +4,12 @@ import {
   CssBaseline,
   Grid,
   Paper,
-  TextField,
   Typography,
   useMediaQuery,
   useTheme,
   CircularProgress,
 } from '@mui/material';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import React from 'react';
 import { useResetPasswordRequestMutation } from '../../../api/auth/api';
 import GradientBackground from '../../../core/components/GradientBackground';
@@ -18,6 +17,7 @@ import GradientButton from '../../../core/components/GradientButton';
 import PublicNavigation from '../../../core/components/PublicNavigation';
 import { emailSchema } from '../validation/email';
 import DisplayError from '../../../core/components/DisplayError';
+import FormikTextField from '../../../core/components/FormikTextField';
 
 const ForgotPassword = () => {
   const theme = useTheme();
@@ -26,6 +26,13 @@ const ForgotPassword = () => {
     { isLoading, isSuccess, isError, error },
   ] = useResetPasswordRequestMutation();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const formik = useFormik({
+    validationSchema: emailSchema,
+    initialValues: { email: '' },
+    onSubmit: async ({ email }) => {
+      await resetPasswordRequest({ email });
+    },
+  });
 
   return (
     <>
@@ -40,65 +47,36 @@ const ForgotPassword = () => {
                 Reset Password
               </Typography>
             </Grid>
-            <Formik
-              validationSchema={emailSchema}
-              initialValues={{ email: '' }}
-              onSubmit={async ({ email }) => {
-                await resetPasswordRequest({ email });
-              }}
-            >
-              {({
-                handleBlur,
-                values,
-                handleChange,
-                errors,
-                touched,
-                handleSubmit,
-              }) => (
-                <form>
-                  <TextField
-                    variant="standard"
-                    margin="normal"
-                    required
-                    fullWidth
-                    autoFocus
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={
-                      errors.email && touched.email ? errors.email : null
-                    }
-                    error={errors.email && touched.email}
-                  />
-                  {isError && <DisplayError error={error} />}
-                  {isSuccess && (
-                    <Typography component="p" mt={1}>
-                      Request sent! Please check your email for further
-                      instructions.
-                    </Typography>
-                  )}
-                  <Box>
-                    <GradientButton
-                      type="submit"
-                      onClick={handleSubmit}
-                      fullWidth
-                      sx={{ mt: 3, mb: 2 }}
-                      startIcon={
-                        isLoading ? (
-                          <CircularProgress color="inherit" size={20} />
-                        ) : null
-                      }
-                    >
-                      Reset Password
-                    </GradientButton>
-                  </Box>
-                </form>
+            <form>
+              <FormikTextField
+                formik={formik}
+                id="email"
+                label="Email Address"
+                required
+              />
+              {isError && <DisplayError error={error} />}
+              {isSuccess && (
+                <Typography component="p" mt={1}>
+                  Request sent! Please check your email for further
+                  instructions.
+                </Typography>
               )}
-            </Formik>
+              <Box>
+                <GradientButton
+                  type="submit"
+                  onClick={formik.handleSubmit}
+                  fullWidth
+                  sx={{ mt: 3, mb: 2 }}
+                  startIcon={
+                    isLoading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null
+                  }
+                >
+                  Reset Password
+                </GradientButton>
+              </Box>
+            </form>
           </Paper>
         </Box>
       </Container>
