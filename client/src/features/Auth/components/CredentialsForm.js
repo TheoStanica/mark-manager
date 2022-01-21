a54080f3b4466dd1f7ace76dd54ba015f8c3878c
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-21 15:30:38 +0200

    refactored component - Formik Component => useFormik

--- client/src/features/Auth/components/CredentialsForm.js
+++ client/src/features/Auth/components/CredentialsForm.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Formik } from 'formik';
-import { TextField, CircularProgress, Box } from '@mui/material';
+import { useFormik } from 'formik';
+import { CircularProgress, Box } from '@mui/material';
 import DisplayError from '../../../core/components/DisplayError';
 import GradientButton from '../../../core/components/GradientButton';
+import FormikTextField from '../../../core/components/FormikTextField';
 
 const CredentialsForm = ({
   validationSchema,
@@ -16,77 +17,45 @@ const CredentialsForm = ({
   successComponent,
   errorComponent,
 }) => {
+  const formik = useFormik({
+    validationSchema,
+    initialValues,
+    onSubmit,
+  });
+
   return (
-    <Formik
-      validationSchema={validationSchema}
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-    >
-      {({
-        handleBlur,
-        values,
-        handleChange,
-        errors,
-        touched,
-        handleSubmit,
-      }) => (
-        <form>
-          <TextField
-            variant="standard"
-            margin="normal"
-            required
-            fullWidth
-            autoFocus
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            helperText={errors.email && touched.email ? errors.email : null}
-            error={errors.email && touched.email}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            variant="standard"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            helperText={
-              errors.password && touched.password ? errors.password : null
-            }
-            error={errors.password && touched.password}
-            sx={{ pb: 1 }}
-          />
-          {isError && <DisplayError error={error} />}
-          {isError && errorComponent}
-          {isSuccess && successComponent}
-          <Box>
-            <GradientButton
-              type="submit"
-              onClick={handleSubmit}
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
-              startIcon={
-                isLoading ? (
-                  <CircularProgress color="inherit" size={20} />
-                ) : null
-              }
-            >
-              {submitText}
-            </GradientButton>
-          </Box>
-        </form>
-      )}
-    </Formik>
+    <form>
+      <FormikTextField
+        formik={formik}
+        id="email"
+        label="Email Address"
+        autoFocus
+        required
+      />
+      <FormikTextField
+        formik={formik}
+        id="password"
+        type="password"
+        required
+        sx={{ pb: 1 }}
+      />
+      {isError && <DisplayError error={error} />}
+      {isError && errorComponent}
+      {isSuccess && successComponent}
+      <Box>
+        <GradientButton
+          type="submit"
+          onClick={formik.handleSubmit}
+          fullWidth
+          sx={{ mt: 3, mb: 2 }}
+          startIcon={
+            isLoading ? <CircularProgress color="inherit" size={20} /> : null
+          }
+        >
+          {submitText}
+        </GradientButton>
+      </Box>
+    </form>
   );
 };
 
