b4b8704cbfa515958fc4a9130f7def4108ffab9c
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-01-29 16:38:48 +0200

    added components to display failure, loading and success

---
+++ client/src/core/components/FetchStatus/Failure.js
@@ -0,0 +1,50 @@
+import { useLottie } from 'lottie-react';
+import React, { useEffect, useState } from 'react';
+import DisplayError from '../DisplayError';
+import { LottieAnimations } from '../../../assets/Lottie';
+import useApplicationTheme from '../../hooks/useApplicationTheme';
+import { Box } from '@mui/material';
+
+const defaultOptions = {
+  animationData: LottieAnimations.errorLight,
+  loop: false,
+  autoPlay: true,
+};
+
+const defaultStyle = {
+  height: 46,
+  width: 46,
+};
+
+const Failure = ({ error, noMessage = false, options, style }) => {
+  const { mode } = useApplicationTheme();
+  const [lottieOptions, setLottieOptions] = useState({
+    ...defaultOptions,
+    ...options,
+  });
+
+  const { View } = useLottie(lottieOptions, { ...defaultStyle, ...style });
+
+  useEffect(() => {
+    let animation = null;
+    if (mode === 'dark') {
+      animation = LottieAnimations.errorDark;
+    } else {
+      animation = LottieAnimations.errorLight;
+    }
+    setLottieOptions((prev) => ({ ...prev, animationData: animation }));
+  }, [mode]);
+
+  return (
+    <>
+      {View}
+      {!noMessage && (
+        <Box sx={{ mt: 1 }}>
+          <DisplayError error={error} simple />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default Failure;
---
+++ client/src/core/components/FetchStatus/Success.js
@@ -0,0 +1,49 @@
+import { useLottie } from 'lottie-react';
+import React, { useEffect, useState } from 'react';
+import { LottieAnimations } from '../../../assets/Lottie';
+import useApplicationTheme from '../../hooks/useApplicationTheme';
+
+const defaultOptions = {
+  animationData: LottieAnimations.successLight,
+  loop: false,
+  autoPlay: true,
+};
+
+const defaultStyle = {
+  height: 58,
+  width: 58,
+};
+
+const Success = ({
+  message = 'Success',
+  noMessage = false,
+  options,
+  style,
+}) => {
+  const { mode } = useApplicationTheme();
+  const [lottieOptions, setLottieOptions] = useState({
+    ...defaultOptions,
+    ...options,
+  });
+
+  const { View } = useLottie(lottieOptions, { ...defaultStyle, ...style });
+
+  useEffect(() => {
+    let animation = null;
+    if (mode === 'dark') {
+      animation = LottieAnimations.successDark;
+    } else {
+      animation = LottieAnimations.successLight;
+    }
+    setLottieOptions((prev) => ({ ...prev, animationData: animation }));
+  }, [mode]);
+
+  return (
+    <>
+      {View}
+      {!noMessage && message}
+    </>
+  );
+};
+
+export default Success;
