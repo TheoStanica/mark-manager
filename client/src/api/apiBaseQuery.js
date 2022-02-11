d0d3bbf4a82e01c0d9e2f97d302a14a6777edbe9
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2022-02-11 16:36:51 +0200

    added axios interceptor, queue for token refresh process

--- client/src/api/apiBaseQuery.js
+++ client/src/api/apiBaseQuery.js
@@ -10,6 +10,20 @@ const apiInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve();
+    }
+  });
+  failedQueue = [];
+};
+
 const makeRequest = ({ urlPrefix, url, method, body, params }) => {
   return apiInstance({
     url: urlPrefix + url,
@@ -22,6 +36,65 @@ const makeRequest = ({ urlPrefix, url, method, body, params }) => {
   });
 };
 
+apiInstance.interceptors.request.use(
+  (config) => {
+    if (store?.getState()?.authSlice.accessToken)
+      config.headers.Authorization = `Bearer ${
+        store?.getState()?.authSlice.accessToken
+      }`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers['Authorization'] = 'Bearer ' + token;
+            return apiInstance(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      return new Promise((resolve, reject) => {
+        apiInstance
+          .post('auth/token', {
+            refreshToken: store?.getState()?.authSlice.refreshToken,
+          })
+          .then(async ({ data }) => {
+            store.dispatch(authSlice.actions.update(data));
+            originalRequest.headers['Authorization'] =
+              'Bearer ' + data.accessToken;
+            processQueue(null, data.refreshToken);
+            resolve(apiInstance(originalRequest));
+          })
+          .catch(async (err) => {
+            store.dispatch(authSlice.actions.clear());
+            processQueue(err, null);
+            reject(err);
+          })
+          .finally(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+    return Promise.reject(error);
+  }
+);
+
 const axiosBaseQuery = ({ urlPrefix }) => async ({
   url,
   method,
@@ -32,32 +105,9 @@ const axiosBaseQuery = ({ urlPrefix }) => async ({
     const result = await makeRequest({ urlPrefix, url, method, body, params });
     return { data: result.data };
   } catch (error) {
-    if (error.response && error.response.status === 401) {
-      try {
-        const refreshResult = await apiInstance.post('auth/token', {
-          refreshToken: store?.getState()?.authSlice.refreshToken,
-        });
-        if (refreshResult.data) {
-          store.dispatch(authSlice.actions.update(refreshResult.data));
-          try {
-            const result = await makeRequest({
-              urlPrefix,
-              url,
-              method,
-              body,
-              params,
-            });
-            return { data: result.data };
-          } catch {}
-        }
-      } catch {
-        store.dispatch(authSlice.actions.clear());
-      }
-    } else {
-      return {
-        error: { status: error.response?.status, data: error.response?.data },
-      };
-    }
+    return {
+      error: { status: error.response?.status, data: error.response?.data },
+    };
   }
 };
 
